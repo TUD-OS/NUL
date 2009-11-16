a74a4bf5de837994e7863e903beb7eef2b6664ce
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2009-11-16 18:54:54 +0100

    Do something trivial in the example roottask.

--- nova/apps/example/main.c
+++ nova/apps/example/main.c
@@ -2,10 +2,20 @@
 
 #include <nova.h>
 
+#define CHECK(x) do { if ((x) != SUCCESS) NOVA_TRAP; } while (0)
+
 int
 start(struct HypervisorInfoPage *hip)
 {
-  /* Empty! */
+  Cap_idx user_cap = hip->sel;
+
+  /* Create a semaphore for fun. */
+  Cap_idx sem = --user_cap;
+  CHECK(create_sm(sem, 0));
+
+  /* Wait for keyboard interrupt and die. */
+  CHECK(semdown(hip->pre + 1));
+
   return 0;
 }
 
