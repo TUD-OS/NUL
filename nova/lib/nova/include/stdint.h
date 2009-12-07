77d27ba3f270ca6e05d9e918e0c966dcd3ad7cc6
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2009-12-07 18:26:02 +0100

    Added uintptr_t type.

--- nova/lib/nova/include/stdint.h
+++ nova/lib/nova/include/stdint.h
@@ -10,6 +10,8 @@ typedef unsigned int       uint32_t;
 typedef unsigned short     uint16_t;
 typedef unsigned char      uint8_t;
 
+typedef uint32_t uintptr_t;
+
 typedef long long int64_t;
 typedef int       int32_t;
 typedef short     int16_t;
