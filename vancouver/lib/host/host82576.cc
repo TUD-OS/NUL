2e3e55d8ef2f8ae5b7c86159791b5a8257151b07
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2009-12-07 18:26:29 +0100

    82576: We can receive packets and sort-of classify them.

--- vancouver/lib/host/host82576.cc
+++ vancouver/lib/host/host82576.cc
@@ -1,11 +1,17 @@
 /**
  * Host Intel 82576 driver.
  *
+ * This device usually comes as a PCIe card with two functions, each
+ * acting as a separate NIC with a few exceptions. There are several
+ * shared resources: EEPROM, PHY(s), Flash. If access to those
+ * resources is desired, the hardware-semaphore of the 82576 has to be
+ * used. We can blissfully ignore it, because we do not need access to
+ * any shared resource.
  */
 
-#include "vmm/motherboard.h"
-#include "host/hostgenericata.h"
-#include "host/hostpci.h"
+#include <vmm/motherboard.h>
+#include <host/hostgenericata.h>
+#include <host/hostpci.h>
 
 // lspci -v:
 // 02:00.0 Ethernet controller: Intel Corporation Device 10c9 (rev 01)
@@ -41,21 +47,39 @@
 // 	Kernel driver in use: igb
 // 	Kernel modules: igb
 
-
-
-// TODO Map registers.
 // TODO Query extended capabilities.
 // TODO MSI support
 
+// 00:1b:21:4d:00:1b
+
+struct EthernetAddr {
+  union {
+    uint64_t raw;
+    uint8_t byte[6];
+  };
+};
+
+#define MAC_FMT "%02x:%02x:%02x:%02x:%02x:%02x"
+#define MAC_SPLIT(x) (x)->byte[0], (x)->byte[1], (x)->byte[2],(x)->byte[3], (x)->byte[4], (x)->byte[5]
+
 class Host82576 : public StaticReceiver<Host82576>
 {
+
+  // Messages are tagged with one or more constants from this
+  // bitfield. You can disable certain kinds of messages in the
+  // constructor.
   enum MessageLevel {
     INFO  = 1<<0,
     DEBUG = 1<<1,
     PCI   = 1<<2,
     IRQ   = 1<<3,
+    RX    = 1<<4,
 
     ALL   = ~0U,
+  };
+
+  enum Spec {
+    RX_QUEUES = 16,
   };
 
   enum Register {
@@ -81,6 +105,9 @@ class Host82576 : public StaticReceiver<Host82576>
     // General
     RCTL      = 0x00100/4,	// RX Control
     RXPBS     = 0x02404/4,	// RX Packet Buffer Size
+
+    RAL0      = 0x05400/4,
+    RAH0      = 0x05400/4,
 
   };
 
@@ -117,16 +144,41 @@ class Host82576 : public StaticReceiver<Host82576>
     SPEED_1000M2  = 0x3,
   };
 
-
   enum Interrupt {
+    IRQ_TXDW      = 1<<7,	// Transmit Descriptor Writeback
     IRQ_LSC       = 1<<2,       // Link Status Change
+    IRQ_RXO       = 1<<6,	// Receiver Overrun
+    IRQ_RXDW      = 1<<7,	// Receive Descriptor Writeback
+    IRQ_VMMB      = 1<<8,	// VM Mailbox/FLR
     IRQ_FER       = 1<<22,      // Fatal Error
     IRQ_NFER      = 1<<23,      // Non-Fatal Error
     IRQ_SWD       = 1<<26,      // Software Watchdog expired
     IRQ_INTA      = 1U<<31,     // INTA asserted (not available for MSIs)
   };
 
+  enum ReceiveControl {
+    RCTL_RXEN     = 1<<1,	// Receive Enable
+    RCTL_SBP      = 1<<2,	// Store Bad Packets
+    RCTL_UPE      = 1<<3,	// Unicast Promiscuous
+    RCTL_MPE      = 1<<4,	// Multicast Promiscuous
+    RCTL_LPE      = 1<<5,	// Long Packet Enable
+    RCTL_BAM      = 1<<15,	// Broadcast Accept Mode
+    RCTL_BSIZE    = 3<<16,	// Buffer Size (default: 0 = 2K)
+  };
+
+  enum PacketType {
+    TYPE_L2       = 1<<11,	// L2 packet
+    TYPE_IPV4     = 1,
+    TYPE_IPV4E    = 1<<1,
+    TYPE_IPV6     = 1<<2,
+    TYPE_IPV6E    = 1<<3,
+    TYPE_TCP      = 1<<4,
+    TYPE_UDP      = 1<<5,
+
+  };
+
   Clock *_clock;
+  DBus<MessageHostOp> &_bus_hostop;
   unsigned long _address;        // Address of PCI config space
   volatile uint32_t *_hwreg;     // Device MMIO registers (128K)
   uint16_t _hwioreg;             // Device I/O  registers (32)
@@ -135,8 +187,125 @@ class Host82576 : public StaticReceiver<Host82576>
   const char *debug_getname() { return "Host82576"; }
 
   unsigned _msg_level;
+  EthernetAddr _mac;
 
-public:
+  class RXQueue {
+    static const unsigned desc_no = 16;
+    Host82576 *_dev;
+    unsigned _no;
+
+    unsigned _last;
+
+  // Advanced Receive Descriptors (7.1.5)
+    struct RXDesc {
+      union {
+	uint64_t word0;
+	uint64_t packet_buf;
+      };
+      union {
+	uint64_t word1;
+	uint64_t header_buf;
+      };
+
+      bool done() { return (word1 & 1) == 1; }
+      uint8_t type() { return (word0 >> 4) & 0x1FFF; }
+    };
+
+    // Descriptors must be 128-byte aligned. (8.10.5)
+    NOVA_ALIGN(128) RXDesc desc[desc_no];
+    struct {
+      NOVA_ALIGN(2) char header[1024];
+      NOVA_ALIGN(2) char packet[2048];
+    } desc_local[desc_no];	// For our personal use
+
+    enum RX {
+      RDBAL  = 0x0C000/4,	// Base Address Low
+      RDBAH  = 0x0C004/4,	// Base Address High
+      RDLEN  = 0x0C008/4,	// Length
+      RDH    = 0x0C010/4,	// Head
+      RDT    = 0x0C018/4,	// Tail
+      RXDCTL = 0x0C028/4,	// Receive Descriptor Control
+      SRRCTL = 0x0C00C/4,	// Split and Replication Receive Control
+      RQDPC  = 0x0C030/4,	// Drop Packet Count
+    };
+
+    enum Control {
+      RXDCTL_ENABLE = 1<<25,	   // Queue Enable
+      SRRCTL_DROP_ENABLE = 1U<<31, // Drop Enable
+      SRRCTL_DESCTYPE = 7<<25,	   // Descriptor Type
+      SRRCTL_DESCTYPE_ADV_1BUF = 1<<25 // One Buffer, No Splitting
+    };
+
+    volatile uint32_t &_rxreg(unsigned reg) { return _dev->_hwreg[0x10*_no + reg]; }
+
+  public:
+
+    void init_slot(unsigned i)
+    {
+      desc[i].header_buf = _dev->to_phys(desc_local[i].header);
+      desc[i].packet_buf = _dev->to_phys(desc_local[i].packet);
+    }
+
+    void enable()
+    {
+      _dev->ensure_rx_enabled();
+      _rxreg(RXDCTL) = RXDCTL_ENABLE;
+
+      for (unsigned i = 0; i < desc_no; i++) {
+	init_slot(i);
+      }
+
+      _last = 0;
+      // XXX Just setting it to 0 does not work. So try it with a
+      // small step first.
+      _rxreg(RDT) = 1;
+      _rxreg(RDT) = 0;		// All buffers valid.
+
+      _dev->msg(Host82576::RX, "RXDCTL[%u] = %x\n", _no, _rxreg(RXDCTL));
+      _dev->msg(Host82576::RX, "SRRCTL[%u] = %x\n", _no, _rxreg(SRRCTL));
+    }
+
+    unsigned dropped() { return _rxreg(RQDPC); }
+
+    void irq(uint32_t cause)
+    {
+      while (desc[_last].done()) {
+	unsigned type = desc[_last].type();
+	_dev->msg(Host82576::RX, "Packet received: %08x %s%s%s%s\n", type,
+		  (type & (TYPE_IPV4|TYPE_IPV4E)) ? "IPv4 " : "",
+		  (type & (TYPE_IPV6|TYPE_IPV6E)) ? "IPv6 " : "",
+		  (type & TYPE_TCP) ? "TCP " : "",
+		  (type & TYPE_UDP) ? "UDP" : "");
+
+	init_slot(_last);
+	_last = (_last+1) % desc_no;
+      }
+
+      _rxreg(RDT) = _last;
+
+      unsigned dropped = _rxreg(RQDPC);
+      if (dropped > 0) _dev->msg(Host82576::RX, "Dropped %u packets.\n", dropped);
+    }
+
+    RXQueue(Host82576 *dev, unsigned no)
+      : _dev(dev), _no(no)
+    {
+      memset(desc, 0, sizeof(desc)); // Just to be sure.
+
+      uint64_t phys_desc = _dev->to_phys(desc);
+      _rxreg(RXDCTL) = 0;	// Disable queue.
+      _rxreg(SRRCTL) = (_rxreg(SRRCTL) & ~SRRCTL_DESCTYPE) |
+	SRRCTL_DESCTYPE_ADV_1BUF | SRRCTL_DROP_ENABLE;
+
+      _rxreg(RDBAL) = phys_desc & 0xFFFFFFFFU;
+      _rxreg(RDBAH) = phys_desc >> 32;
+      _rxreg(RDLEN) = sizeof(RXDesc)*desc_no;
+
+      _dev->msg(Host82576::RX, "RX queue %u initialized with %u descriptors (%u bytes).\n", no,
+		desc_no, _rxreg(RDLEN));
+
+    }
+  } *_rx;
 
   __attribute__ ((format (printf, 3, 4)))
   void msg(unsigned level, const char *msg, ...)
@@ -150,20 +319,18 @@ public:
     }
   }
 
-  bool receive(MessageIrq &irq_msg)
+  uintptr_t to_phys(void *ptr) {
+    MessageHostOp msg(MessageHostOp::OP_VIRT_TO_PHYS, reinterpret_cast<unsigned long>(ptr));
+    if (_bus_hostop.send(msg) && msg.phys) {
+      return msg.phys;
+    } else Logging::panic("Could not resolve %p's physical address.\n", ptr);
+  }
+
+  void ensure_rx_enabled()
   {
-    if (irq_msg.line != _hostirq || irq_msg.type != MessageIrq::ASSERT_IRQ)  return false;
+    _hwreg[RCTL] |= RCTL_RXEN;
+  }
 
-    uint32_t irq_cause = _hwreg[ICR];
-    if (irq_cause == 0) return false;
-
-    msg(IRQ, "IRQ! ICR = %x\n", irq_cause);
-
-    // XXX Do something
-    log_device_status();
-
-    return true;
-  };
 
   void log_device_status()
   {
@@ -197,8 +364,56 @@ public:
     return true;
   }
 
+public:
+
+  EthernetAddr ethernet_address()
+  {
+    // Hardware initializes the Receive Address registers of queue 0
+    // with the MAC address stored in the EEPROM.
+    EthernetAddr a = {{ _hwreg[RAL0] | (((uint64_t)_hwreg[RAH0] & 0xFFFF) << 32) }};
+
+    if (((_address >> 8) & 0xF) == 1)
+      a.byte[5] ^= 1;
+
+    return a;
+  }
+
+  bool receive(MessageIrq &irq_msg)
+  {
+    if (irq_msg.line != _hostirq || irq_msg.type != MessageIrq::ASSERT_IRQ)  return false;
+
+    uint32_t irq_cause = _hwreg[ICR];
+    if (irq_cause == 0) return false;
+
+    msg(IRQ, "IRQ! ICR = %x\n", irq_cause);
+
+    if (irq_cause & (IRQ_RXDW | IRQ_RXO)) {
+      _rx->irq(irq_cause);
+    }
+
+    if (irq_cause & IRQ_LSC) {
+      bool gone_up = (_hwreg[STATUS] & STATUS_LU) != 0;
+      msg(IRQ, "Link status changed to %s.\n", gone_up ? "UP" : "DOWN");
+      if (gone_up) {
+	// XXX Enable promiscuous mode for now.
+	_hwreg[RCTL] = RCTL_RXEN | RCTL_BAM | RCTL_UPE | RCTL_MPE;
+	_rx->enable();
+	msg(RX | IRQ, "RX enabled.\n");
+      } else {
+	// Down
+	msg(RX | IRQ, "RX disabled.\n");
+	_hwreg[RCTL] &= ~RCTL_RXEN;
+      }
+    }
+
+    // XXX Do something
+    log_device_status();
+
+    return true;
+  };
+
   Host82576(HostPci &pci, DBus<MessageHostOp> &bus_hostop, Clock *clock, unsigned long address, unsigned hostirq)
-    : _clock(clock), _address(address), _hostirq(hostirq), _msg_level(ALL & ~PCI)
+    : _clock(clock), _bus_hostop(bus_hostop), _address(address), _hostirq(hostirq), _msg_level(ALL & ~PCI)
   {
     msg(INFO, "Found Intel 82576-style controller at %lx. Attaching IRQ %x.\n", address, hostirq);
 
@@ -224,7 +439,7 @@ public:
 	  if (!bus_hostop.send(iomsg))
 	    Logging::panic("%s could not allocate I/O window.\n", __PRETTY_FUNCTION__);
 
-	  msg(INFO, "Found I/O window at %x.\n", _hwioreg);
+	  msg(INFO, "Found  I/O window at %x.\n", _hwioreg);
 	}
       } else {
 	// Memory BAR
@@ -250,7 +465,6 @@ public:
       Logging::panic("%s could not find its register windows.\n", __PRETTY_FUNCTION__);
 
     /// Initialization (4.5)
-    // XXX Does this apply to one or both functions?
     msg(INFO, "Perform Global Reset.\n");
     // Disable interrupts
     _hwreg[IMS] = 0;
@@ -281,6 +495,13 @@ public:
 
     // XXX Set CTRL.RFCE/CTRL.TFCE
 
+    _mac = ethernet_address();
+    msg(INFO, "We are " MAC_FMT "\n", MAC_SPLIT(&_mac));
+
+    // Allocating an RX queue for our personal use.
+
+    _rx = new RXQueue(this, 0);
+
     // IRQ
     MessageHostOp irq_msg(MessageHostOp::OP_ATTACH_HOSTIRQ, hostirq);
     if (!(irq_msg.value == ~0U || bus_hostop.send(irq_msg)))
@@ -288,7 +509,8 @@ public:
     msg(IRQ, "Attached to IRQ. Waiting for link to come up.\n");
 
     // Wait for Link Up event
-    _hwreg[IMS] = IRQ_LSC | IRQ_FER | IRQ_NFER;
+    //_hwreg[IMS] = IRQ_LSC | IRQ_FER | IRQ_NFER | IRQ_RXDW | IRQ_RXO | IRQ_TXDW;
+    _hwreg[IMS] = ~0U;
 
     // Capabilities
     // unsigned long sriov_cap = pci.find_extended_cap(_address, 0x160);
@@ -297,23 +519,22 @@ public:
   }
 };
 
-PARAM(host82576,
-      {
-	unsigned irqline;
-	unsigned irqpin;
-	HostPci pci(mb.bus_hwpcicfg);
-	unsigned found = 0;
+PARAM(host82576, {
+    unsigned irqline;
+    unsigned irqpin;
+    HostPci pci(mb.bus_hwpcicfg);
+    unsigned found = 0;
 
-	for (unsigned address, num = 0; (address = pci.search_device(0x2, 0x0, num++, irqline, irqpin));) {
-	  unsigned cfg0 = pci.conf_read(address + 0x0);
-	  if (cfg0 == 0x10c98086) {
-	    if (found++ == argv[0]) {
-	      Host82576 *dev = new Host82576(pci, mb.bus_hostop, mb.clock(), address, argv[1]);
-	      mb.bus_hostirq.add(dev, &Host82576::receive_static<MessageIrq>);
-	    }
-	  }
+    for (unsigned address, num = 0; (address = pci.search_device(0x2, 0x0, num++, irqline, irqpin));) {
+      unsigned cfg0 = pci.conf_read(address + 0x0);
+      if (cfg0 == 0x10c98086) {
+	if (found++ == argv[0]) {
+	  Host82576 *dev = new Host82576(pci, mb.bus_hostop, mb.clock(), address, argv[1]);
+	  mb.bus_hostirq.add(dev, &Host82576::receive_static<MessageIrq>);
 	}
-      },
-      "host82576:func,irq - provide driver for Intel 82576 Ethernet controller.");
+      }
+    }
+  },
+  "host82576:func,irq - provide driver for Intel 82576 Ethernet controller.");
 
 // EOF
