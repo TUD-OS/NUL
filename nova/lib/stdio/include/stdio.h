c3ba8f7b88d471616550b73d663015a3c241a6c1
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2009-12-16 18:00:10 +0100

    Mark printf prototypes with magic gcc declarations.

--- nova/lib/stdio/include/stdio.h
+++ nova/lib/stdio/include/stdio.h
@@ -5,10 +5,10 @@
 #include <stddef.h>
 #include <stdarg.h>
 
-int printf(const char *format, ...);
+int printf(const char *format, ...) __attribute__ ((format(printf, 1, 2)));;
 int vprintf(const char *format, va_list ap);
 
-int snprintf(char *str, size_t size, const char *format, ...);
+int snprintf(char *str, size_t size, const char *format, ...) __attribute__ ((format(printf, 3, 4)));;
 int vsnprintf(char *str, size_t size, const char *format, va_list ap);
 
 int putchar(int c);		/* Not implemented by libstdio */
