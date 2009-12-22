d08b65e2f92f8ba975425cdebbb27e8f11438672
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2009-12-22 16:58:00 +0100

    add copyright

--- vancouver/executor/insthelper.h
+++ vancouver/executor/insthelper.h
@@ -1,3 +1,20 @@
+/**
+ * Instruction helper.
+ *
+ * Copyright (C) 2009, Bernhard Kauer <bk@vmmon.org>
+ *
+ * This file is part of Vancouver.
+ *
+ * Vancouver is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Vancouver is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
   /**
    * Add base and check segment type and limit.
    */
