c6375c4a1eec04db40888f76933c0e92f65454dc
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2009-12-23 11:01:00 +0100

    hostserial should do the output as well

--- vancouver/host/hostserial.cc
+++ vancouver/host/hostserial.cc
@@ -62,6 +62,23 @@ class HostSerial : public StaticReceiver<HostSerial>
       }
     return false;
   }
+
+
+  bool  receive(MessageSerial &msg)
+  {
+    if (msg.serial != _serialdev + 1)  return false;
+    for (unsigned i=0; i < 10000; i++)
+      {
+	if (inb(_base+0xf) & 0x20)
+	  {
+	    outb(_base, msg.ch);
+	    break;
+	  }
+	Cpu::pause();
+      }
+    return true;
+  }
+
   
   HostSerial(DBus<MessageIOIn> &bus_hwioin, DBus<MessageIOOut> &bus_hwioout, DBus<MessageSerial> &bus_serial,
 	     unsigned serialdev, unsigned base, unsigned irq, unsigned speed, unsigned lcr, unsigned ier)
@@ -92,20 +109,34 @@ class HostSerial : public StaticReceiver<HostSerial>
 
 PARAM(hostserial,
       {
-	MessageHostOp msg1(MessageHostOp::OP_ALLOC_IOIO_REGION, (argv[1] << 8) |  3);
+	unsigned iobase = argv[1];
+
+	if (iobase == ~0u)
+	  {
+	    MessageHostOp msg(MessageHostOp::OP_ALLOC_IOMEM, 0, 0x1000);
+	    if (mb.bus_hostop.send(msg) && msg.ptr)
+	      {
+		iobase = *reinterpret_cast<unsigned short *>(msg.ptr + 0x400);
+		//XXX unmap(msg.ptr, 1<<12);
+	      }
+	  }
+	MessageHostOp msg1(MessageHostOp::OP_ALLOC_IOIO_REGION,  (iobase << 8) |  3);
 	if (!mb.bus_hostop.send(msg1))
 	  Logging::panic("%s failed to allocate ports %lx+8\n", __PRETTY_FUNCTION__, argv[1]);
 
 	Device *dev = new HostSerial(mb.bus_hwioin, mb.bus_hwioout, mb.bus_serial, 
-				     argv[0], argv[1], argv[2], argv[3] == ~0UL ? 115200 : argv[3]  , argv[4] == ~0UL ? 3 : argv[4], argv[2] != ~0U);
+				     argv[0], iobase, argv[2], argv[3] == ~0UL ? 115200 : argv[3]  , argv[4] == ~0UL ? 3 : argv[4], argv[2] != ~0U);
 	mb.bus_hostirq.add(dev, &HostSerial::receive_static<MessageIrq>);
+	mb.bus_serial.add(dev, &HostSerial::receive_static<MessageSerial>);
 
 	MessageHostOp msg2(MessageHostOp::OP_ATTACH_HOSTIRQ, argv[2]);
 	if (!(msg2.value == ~0U || mb.bus_hostop.send(msg2)))
 	  Logging::panic("%s failed to attach hostirq %lx\n", __PRETTY_FUNCTION__, argv[2]);
 
       },
-      "hostserial:hostdevnr,hostiobase,hostirq,speed=115200,lcr=3 - provide an input backend from the host serial port.",
+      "hostserial:hostdevnr,hostiobase,hostirq,speed=115200,lcr=3 - provide an in+output backend from the host serial port.",
       "Example:   'hostserial:0x4711,0x3f8,4,115200,3'.",
+      "If no iobase is given, the first serial port from the BIOS is used.",
       "The lcr is used to define word-length, length of stop-bit and parity.",
-      "See the LCR encoding of the 16550. The default is lcr=3, which means 8N1.");
+      "See the LCR encoding of the 16550. The default is lcr=3, which means 8N1.",
+      "The output is received from hostdevnr+1.");
