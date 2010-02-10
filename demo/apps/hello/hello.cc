7339fa935d1c92a16299b1867a101037fbc42826
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-02-11 00:37:05 +0100

    add simple application to burn time

---
+++ demo/apps/hello/hello.cc
@@ -0,0 +1,37 @@
+/*
+ * Test application that burns CPU time.
+ *
+ * Copyright (C) 2010, Bernhard Kauer <bk@vmmon.org>
+ *
+ * This file is part of Vancouver.nova.
+ *
+ * Vancouver.nova is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * Vancouver.nova is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
+#include "sigma0/console.h"
+
+class Hello : public ProgramConsole
+{
+public:
+  static void exit(unsigned long status)
+  {
+    Logging::printf("%s(%lx)\n", __func__, status);
+  }
+
+  int run(Hip *hip)
+  {
+    console_init("Hello");;
+    for (unsigned i=0; ; i++)
+      Logging::printf("%8x Hello World!\n", i);
+    return 0;
+  }
+};
+
+ASMFUNCS(Hello);
