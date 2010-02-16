253e4a46e10129d1a44e01e49abf207997e13323
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2010-02-16 17:42:06 +0100

    Forth demo application.

---
+++ demo/apps/forth/forth.cc
@@ -0,0 +1,29 @@
+#include <ficl.h>
+#include "sigma0/console.h"
+
+extern "C" int
+putchar(int c)
+{
+  Logging::printf("%c", c);
+  return c;
+}
+
+class Forth : public ProgramConsole
+{
+public:
+  int run(Hip *hip)
+  {
+    console_init("Forth");;
+    Logging::printf("Forth booting...\n");
+    
+    FICL_SYSTEM *pSys = ficlInitSystem(10000);
+    // XXX Add new words here.
+    FICL_VM *pVM = ficlNewVM(pSys);
+
+    ficlEvaluate(pVM, ".ver .( " __DATE__ " ) cr quit");
+
+    return 0;
+  }
+};
+
+ASMFUNCS(Forth, NovaProgram);
