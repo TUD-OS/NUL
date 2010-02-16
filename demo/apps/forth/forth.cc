62973c5e06726ec58c985e5c1ae82fdafc0cfded
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2010-02-16 19:32:47 +0100

    Debugging IO

--- demo/apps/forth/forth.cc
+++ demo/apps/forth/forth.cc
@@ -1,6 +1,8 @@
 #include <ficl.h>
 #include "sigma0/console.h"
 
+#include <stdio.h>
+
 extern "C" int
 putchar(int c)
 {
@@ -14,11 +16,13 @@ public:
   int run(Hip *hip)
   {
     console_init("Forth");
-    Logging::printf("Forth booting...\n");
-    
+    printf("Forth booting...\n");
+
     FICL_SYSTEM *pSys = ficlInitSystem(10000);
+    printf("ficlInitSystem OK -> %p\n", pSys);
     // XXX Add new words here.
     FICL_VM *pVM = ficlNewVM(pSys);
+    printf("ficlNewVM OK -> %p\n", pVM);
 
     ficlEvaluate(pVM, ".ver .( " __DATE__ " ) cr quit");
 
