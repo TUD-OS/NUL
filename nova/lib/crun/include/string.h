9dab3983370a9aeacf0211be6a7623e161e54c17
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2010-02-22 18:23:00 +0100

    Don't call __builtin_foo directly. This just leads to endless loops if GCC decides not to insert the string op inline.

--- nova/lib/crun/include/string.h
+++ nova/lib/crun/include/string.h
@@ -28,194 +28,37 @@
 # error Unknown platform.
 #endif
 
-#ifndef STRING_INLINE
-# define STRING_INLINE static inline
-#endif
-
 #ifdef __cplusplus
-# define STRING_CAST(type, expr) (reinterpret_cast<type>(expr))
+# define EXTERN_C extern "C"
 #else
-# define STRING_CAST(type, expr) ((type)(expr))
+# define EXTERN_C
 #endif
 
-STRING_INLINE void *
-memcpy(void *dst, const void *src, size_t count)
-{
-  return __builtin_memcpy(dst, src, count);
-}
+EXTERN_C void *memcpy(void *dst, const void *src, size_t count);
+
+EXTERN_C void *memmove(void *dst, const void *src, size_t count);
+EXTERN_C int memcmp(const void *dst, const void *src, size_t count);
+EXTERN_C void *memset(void *dst, int n, size_t count);
+EXTERN_C size_t strnlen(const char *src, size_t maxlen);
+EXTERN_C size_t strlen(const char *src);
+EXTERN_C char *strstr(char *haystack, const char *needle);
+
+/* XXX Belongs in stdlib.h */
+EXTERN_C unsigned long strtoul(char *nptr, char **endptr, int base);
+
+EXTERN_C const char *strchr(const char *s, int c);
+EXTERN_C char *strsep(char **stringp, const char *delim);
+EXTERN_C char *strcpy(char *dst, const char *src);
+EXTERN_C char *strncpy(char *dst, const char *src, size_t n);
+EXTERN_C int strcmp(const char *dst, const char *src);
+EXTERN_C char *strcat(char *dst, const char *src);
 
 /** "Fast" memcpy for double-word aligned buffers. */
-STRING_INLINE void *
+static inline void *
 memcpyl(void *dst, const void *src, size_t count)
 {
   asm volatile ("rep movsl" : "+D"(dst), "+S"(src), "+c" (count) : : "memory");
   return dst;
-}
-
-
-
-STRING_INLINE void *
-memmove(void *dst, const void *src, size_t count)
-{
-  char *d = STRING_CAST(char *, dst);
-  const char *s = STRING_CAST(const char *, src);
-  if (d > s)
-    {
-      d+=count-1;
-      s+=count-1;
-      asm volatile ("std; rep movsb; cld;" : "+D"(d), "+S"(s), "+c"(count) : : "memory");
-    }
-  else
-    asm volatile ("rep movsb" : "+D"(d), "+S"(s), "+c"(count) :  : "memory");
-  return dst;
-}
-
-STRING_INLINE int
-memcmp(const void *dst, const void *src, size_t count)
-{
-  return __builtin_memcmp(dst, src, count);
-}
-
-
-STRING_INLINE void *
-memset(void *dst, int n, size_t count)
-{
-  void *res = dst;
-  asm volatile ("rep stosb" : "+D"(dst), "+a"(n), "+c"(count) :  : "memory");
-  return res;
-}
-
-
-STRING_INLINE size_t
-strnlen(const char *src, size_t maxlen)
-{
-  unsigned long count = maxlen;
-  unsigned char ch = 0;
-  asm volatile ("repne scasb; setz %0;" : "+a"(ch), "+D"(src), "+c"(count));
-  if (ch) count--;
-  return maxlen - count;
-}
-
-
-STRING_INLINE size_t
-strlen(const char *src)
-{
-  return __builtin_strlen(src);
-}
-
-
-STRING_INLINE char *
-strstr(char *haystack, const char *needle)
-{
-  int index;
-  do {
-    for (index=0; needle[index] == haystack[index] && needle[index]; index++)
-      ;
-    if (!needle[index])
-      return haystack;
-    haystack += index ? index : 1;
-  } while (*haystack);
-  return 0;
-
-}
-
-STRING_INLINE unsigned long
-strtoul(char *nptr, char **endptr, int base)
-{
-  unsigned long res = 0;
-  if (*nptr == '0' && *(nptr+1) == 'x')
-    {
-      nptr += 2;
-      base = base ? base : 16;
-    }
-  else if (*nptr == '0')
-    base = base ? base : 8;
-  else
-    base = base ? base : 10;
-
-  while (*nptr)
-    {
-      long val = *nptr - '0';
-      if (val > 9)
-	val = val - 'a' + '0' + 10;
-      if (val < 0 || val > base)
-	break;
-      res = res*base + val;
-      nptr++;
-    }
-  if (endptr) *endptr = nptr;
-  return res;
-}
-
-STRING_INLINE
-const char *
-strchr(const char *s, int c)
-{
-  while (*s)
-    if (c == *s)
-      return s;
-    else s++;
-  return 0;
-}
-
-STRING_INLINE
-char *
-strsep(char **stringp, const char *delim)
-{
-  if (!stringp || !*stringp)
-    return 0;
-  char *res = *stringp;
-  char *s = res;
-  while (*s)
-    {
-      if (strchr(delim, *s))
-	{
-	  *s = 0;
-	  *stringp = s+1;
-	  break;
-	}
-      s++;
-    }
-  if (res == *stringp)
-    *stringp = 0;
-  return res;
-
-}
-
-STRING_INLINE
-char *
-strcpy(char *dst, const char *src)
-{
-  char *r = dst;
-  asm volatile ("1: lodsb; test %%al, %%al; stosb; jnz 1b;  " : "+D" (dst), "+S"(src) : : "eax", "memory", "cc");
-  return r;
-}
-
-STRING_INLINE
-char *
-strncpy(char *dst, const char *src, size_t n)
-{
-  char *r = dst;
-  asm volatile ("1: jecxz 2f; dec %%ecx; lodsb; test %%al, %%al; stosb; jnz 1b; 2:"
-		: "+D" (dst), "+S" (src), "+c" (n) : : "eax", "memory", "cc");
-  return r;
-}
-
-STRING_INLINE
-unsigned
-strcmp(const char *dst, const char *src) 
-{
-  return memcmp(dst, src, strlen(dst));
-}
-
-STRING_INLINE
-char *
-strcat(char *dst, const char *src)
-{
-  char *r = dst;
-  while (*dst != 0) dst++;
-  strcpy(dst, src);
-  return r;
 }
 
 /* EOF */
--- nova/lib/crun/string.c
+++ nova/lib/crun/string.c
@@ -5,3 +5,149 @@
 /* We don't need extern "C", because we compile the non-inline version
    from C only. For the inline version, it does not matter. */
 #include <string.h>
+
+void *
+memcpy(void *dst, const void *src, size_t count)
+{
+  void *ret = dst;
+  asm volatile ("rep movsb" : "+D"(dst), "+S"(src), "+c" (count) : : "memory");
+  return ret;
+}
+
+void *
+memmove(void *dst, const void *src, size_t count)
+{
+  char *d = dst;
+  const char *s = src;
+  if (d > s) {
+    d+=count-1;
+    s+=count-1;
+    asm volatile ("std; rep movsb; cld;" : "+D"(d), "+S"(s), "+c"(count) : : "memory");
+  } else
+    asm volatile ("rep movsb" : "+D"(d), "+S"(s), "+c"(count) :  : "memory");
+  return dst;
+}
+
+void *
+memset(void *dst, int n, size_t count)
+{
+  void *res = dst;
+  asm volatile ("rep stosb" : "+D"(dst), "+a"(n), "+c"(count) :  : "memory");
+  return res;
+}
+
+size_t
+strnlen(const char *src, size_t maxlen)
+{
+  unsigned long count = maxlen;
+  unsigned char ch = 0;
+  asm volatile ("repne scasb; setz %0;" : "+a"(ch), "+D"(src), "+c"(count));
+  if (ch) count--;
+  return maxlen - count;
+}
+
+char *
+strstr(char *haystack, const char *needle)
+{
+  int index;
+  do {
+    for (index=0; needle[index] == haystack[index] && needle[index]; index++)
+      ;
+    if (!needle[index])
+      return haystack;
+    haystack += index ? index : 1;
+  } while (*haystack);
+  return 0;
+}
+
+unsigned long
+strtoul(char *nptr, char **endptr, int base)
+{
+  unsigned long res = 0;
+  if (*nptr == '0' && *(nptr+1) == 'x')
+    {
+      nptr += 2;
+      base = base ? base : 16;
+    }
+  else if (*nptr == '0')
+    base = base ? base : 8;
+  else
+    base = base ? base : 10;
+
+  while (*nptr)
+    {
+      long val = *nptr - '0';
+      if (val > 9)
+	val = val - 'a' + '0' + 10;
+      if (val < 0 || val > base)
+	break;
+      res = res*base + val;
+      nptr++;
+    }
+  if (endptr) *endptr = nptr;
+  return res;
+}
+
+const char *
+strchr(const char *s, int c)
+{
+  while (*s)
+    if (c == *s)
+      return s;
+    else s++;
+  return 0;
+}
+
+char *
+strsep(char **stringp, const char *delim)
+{
+  if (!stringp || !*stringp)
+    return 0;
+  char *res = *stringp;
+  char *s = res;
+  while (*s) {
+    if (strchr(delim, *s)) {
+      *s = 0;
+      *stringp = s+1;
+      break;
+    }
+    s++;
+  }
+  if (res == *stringp)
+    *stringp = 0;
+  return res;
+}
+
+char *
+strcpy(char *dst, const char *src)
+{
+  char *r = dst;
+  asm volatile ("1: lodsb; test %%al, %%al; stosb; jnz 1b;  " : "+D" (dst), "+S"(src) : : "eax", "memory", "cc");
+  return r;
+}
+
+char *
+strncpy(char *dst, const char *src, size_t n)
+{
+  char *r = dst;
+  asm volatile ("1: jecxz 2f; dec %%ecx; lodsb; test %%al, %%al; stosb; jnz 1b; 2:"
+		: "+D" (dst), "+S" (src), "+c" (n) : : "eax", "memory", "cc");
+  return r;
+}
+
+char *
+strcat(char *dst, const char *src)
+{
+  char *r = dst;
+  while (*dst != 0) dst++;
+  strcpy(dst, src);
+  return r;
+}
+
+int
+strcmp(const char *dst, const char *src) 
+{
+  return memcmp(dst, src, strlen(dst));
+}
+
+/* EOF */
