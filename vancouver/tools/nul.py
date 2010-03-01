0fd9519e95494f90ba9b51aa52b762f390bb8536
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-03-01 19:00:38 +0100

    we still need cstdint

--- vancouver/tools/nul.py
+++ vancouver/tools/nul.py
@@ -28,7 +28,7 @@ def LibEnv(tenv, libs):
         env.Append(CPPPATH = guess_include(lib))
     return env
 
-def App(tenv, name, SOURCES = [], INCLUDE = [], LIBS = [ 'crun' ],
+def App(tenv, name, SOURCES = [], INCLUDE = [], LIBS = [],
         LINKSCRIPT = "#vsys/linker.ld", MEMSIZE = 1<<23):
     env = LibEnv(tenv, INCLUDE)
     env = AppEnv(env,  LIBS, MEMSIZE)
@@ -36,7 +36,7 @@ def App(tenv, name, SOURCES = [], INCLUDE = [], LIBS = [ 'crun' ],
                     SOURCES,
                     linkscript = LINKSCRIPT)
 
-def Lib(tenv, name, SOURCES = [], INCLUDE = [], LIBS = [ 'crun' ]):
+def Lib(tenv, name, SOURCES = [], INCLUDE = [], LIBS = []):
     env = LibEnv(tenv, INCLUDE + LIBS + [ name ])
     return env.StaticLibrary(output + "/lib/%s" % name,
                              SOURCES)
