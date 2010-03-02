97b9047e01a08874eb8965e19536c68fae11c86b
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-03-02 21:08:44 +0100

    first try

--- vancouver/model/pcidirect.cc
+++ vancouver/model/pcidirect.cc
@@ -41,62 +41,87 @@ class DirectPciDevice : public StaticReceiver<DirectPciDevice>, public HostPci
   unsigned  _irq_count;
   unsigned *_host_irqs;
   MsiXTableEntry *_msix_table;
+  MsiXTableEntry *_msix_host_table;
   unsigned  _cfgspace[PCI_CFG_SPACE_DWORDS];
   unsigned  _bar_count;
   unsigned  _msi_cap;
   bool      _msi_64bit;
   unsigned  _msix_cap;
-  long long unsigned _bases[MAX_BAR];
-  long long unsigned _sizes[MAX_BAR];
+  unsigned  _msix_bar;
+  struct {
+    unsigned long size;
+    char *   ptr;
+    bool     io;
+    unsigned short port;
+  } _barinfo[MAX_BAR];
 
   const char *debug_getname() { return "DirectPciDevice"; }
+
 
   /**
    * Map the bars.
    */
-  void map_bars() {
+  void map_bars(unsigned long long *bases, unsigned long long *sizes) {
     for (unsigned i=0; i < _bar_count; i++) {
+      _barinfo[i].size = sizes[i];
+      if (!bases[i]) continue;
+      if ((bases[i] & 1) == 1) {
+	_barinfo[i].io   = true;
+	_barinfo[i].port = bases[i] & BAR_IO_MASK;
 
-      unsigned long bar = _bases[i];
-      if (!bar) continue;
-
-      if ((bar & 1) == 1) {
-	MessageHostOp msg(MessageHostOp::OP_ALLOC_IOIO_REGION, ((bar & BAR_IO_MASK) << 8) |  Cpu::bsr(_sizes[i] | 0x3));
+	MessageHostOp msg(MessageHostOp::OP_ALLOC_IOIO_REGION, (_barinfo[i].port << 8) |  Cpu::bsr(sizes[i] | 0x3));
 	_mb.bus_hostop.send(msg);
       } else {
+	_barinfo[i].io  = false;
 
-	MessageHostOp msg(MessageHostOp::OP_ALLOC_IOMEM, bar & ~0x1f, 1 << Cpu::bsr(((_sizes[i] - 1) | 0xfff) + 1));
+	MessageHostOp msg(MessageHostOp::OP_ALLOC_IOMEM, bases[i] & ~0x1f, 1 << Cpu::bsr(((sizes[i] - 1) | 0xfff) + 1));
 	if (_mb.bus_hostop.send(msg) && msg.ptr)
-	  _bases[i] = reinterpret_cast<unsigned long>(msg.ptr) + (bar & 0x10);
+	  _barinfo[i].ptr = msg.ptr + (bases[i] & 0x10);
 	else
 	  Logging::panic("can not map IOMEM region %lx+%x", msg.value, msg.len);
 
       }
-      Logging::printf("%s() bar %lx -> %llx size %llx\n", __func__, bar, _bases[i], _sizes[i]);
     }
   }
 
+
+  bool match_iobars(unsigned short port, unsigned short &newport) {
+
+    // optimize access
+    if (port < 0x100) return false;
+
+    // check whether io decode is disabled
+    if (~_cfgspace[1] & 1) return false;
+    for (unsigned i=0; i < _bar_count; i++) {
+      if (!_barinfo[i].io || (_cfgspace[4 + i] ^ port) & ~0x3u) continue;
+      newport = _barinfo[i].port;
+      return true;
+    }
+    return false;
+  }
+
   /**
-   * Check whether the guest io address matches and translate to host
+   * Check whether the guest mem address matches and translate to host pointer
    * address.
    */
-  unsigned match_bars(unsigned &address, unsigned size, bool iospace)
-  {
-    if (iospace && address < 0x100) return false;
+  unsigned match_bars(unsigned long address, unsigned size, char *&ptr) {
 
     COUNTER_INC("PCIDirect::match");
-    // check whether io decode is disabled
-    if (iospace && ~_cfgspace[1] & 1 || !iospace && ~_cfgspace[1] & 2)  return 0;
-    for (unsigned i=0; i < _bar_count; i++)
-      {
-	unsigned  bar = _cfgspace[4 + i];
-	// mask prefetch bit
-	if (!iospace) bar &= ~0xc;
-	if (_sizes[i] || (bar & 1) != iospace || !in_range(address, bar & ~0x3, _sizes[i] + 1 - size + 1))
-	  continue;
-	address = address - bar + _bases[i];
-	return 4 + i;
-      }
+
+    // mem decode is disabled?
+    if (~_cfgspace[1] & 2)  return 0;
+
+
+    for (unsigned i=0; i < _bar_count; i++) {
+
+      // we assume that accesses with a size larger than the bar will never happen
+      if (_barinfo[i].size < size || _barinfo[i].io || !in_range(address, _cfgspace[4 + i] & BAR_MEM_MASK, _barinfo[i].size - size))
+	continue;
+      ptr = _barinfo[i].ptr + address - (_cfgspace[4 + i] & BAR_MEM_MASK);
+      if (_msix_host_table && ptr >= reinterpret_cast<char *>(_msix_host_table) && ptr < reinterpret_cast<char *>(_msix_host_table + _irq_count))
+	ptr = reinterpret_cast<char *>(_msix_table) + (reinterpret_cast<char *>(_msix_host_table) - ptr);
+      return 4 + i;
+    }
     return 0;
   }
 
@@ -106,9 +131,7 @@ class DirectPciDevice : public StaticReceiver<DirectPciDevice>, public HostPci
   bool receive(MessageIOIn &msg)
   {
     unsigned old_port = msg.port;
-    unsigned new_port = msg.port;
-    if (!match_bars(new_port, 1 << msg.type, true))  return false;
-    msg.port = new_port;
+    if (!match_iobars(old_port, msg.port))  return false;
     bool res = _mb.bus_hwioin.send(msg);
     msg.port = old_port;
     return res;
@@ -119,7 +142,7 @@ class DirectPciDevice : public StaticReceiver<DirectPciDevice>, public HostPci
   {
     unsigned old_port = msg.port;
     unsigned new_port = msg.port;
-    if (!match_bars(new_port, 1 << msg.type, true))  return false;
+    if (!match_iobars(old_port, msg.port))  return false;
     msg.port = new_port;
     bool res = _mb.bus_hwioout.send(msg);
     msg.port = old_port;
@@ -147,20 +170,25 @@ class DirectPciDevice : public StaticReceiver<DirectPciDevice>, public HostPci
 	  }
 	else
 	  {
-	    //unsigned old = _cfgspace[msg.offset >> 2];
 	    unsigned mask = ~0u;
-	    if (in_range(msg.offset, 0x10, MAX_BAR * 4))  mask &= _sizes[(msg.offset - 0x10) >> 2] - 1;
-	    else if (_msi_cap) {
+	    if (in_range(msg.offset, 0x10, MAX_BAR * 4)) {
+	      if (_barinfo[(msg.offset - 0x10) >> 2].size)
+		mask = _barinfo[(msg.offset - 0x10) >> 2].size - 1;
+	      else
+		mask = 0;
+	    }
+	    if (_msi_cap) {
 	      if (msg.offset == _msi_cap) mask = 0x710000;
 	      else if (msg.offset == _msi_cap + 4) mask = ~3;
 	      else if (msg.offset == _msi_cap + (_msi_64bit ? 12 : 8)) mask = 0xffff;
 	    }
-	    _cfgspace[msg.offset >> 2] = _cfgspace[msg.offset >> 2] & ~mask | msg.value & mask;
-
-	    //write through if not in the bar or msi range
-	    if (!in_range(msg.offset, 0x10, MAX_BAR * 4) && !(_msi_cap && in_range(msg.offset, _msi_cap, _msi_64bit ? 16 : 12)))
+	    if (~mask)
+	      _cfgspace[msg.offset >> 2] = _cfgspace[msg.offset >> 2] & ~mask | msg.value & mask;
+	    else {
+	      //write through if not in the BAR or MSI range
 	      conf_write(_bdf, msg.offset, _cfgspace[msg.offset >> 2]);
-
+	      _cfgspace[msg.offset >> 2] = conf_read(_bdf, msg.offset);
+	    }
 	    //Logging::printf("%s:%x -- %8x,%8x old %8x\n", __PRETTY_FUNCTION__, _bdf, msg.offset, _cfgspace[msg.offset >> 2], old);
 	  return true;
 	  }
@@ -207,13 +235,13 @@ class DirectPciDevice : public StaticReceiver<DirectPciDevice>, public HostPci
 
   bool receive(MessageIrqNotify &msg)
   {
+    // XXX
     unsigned irq = _cfgspace[15] & 0xff;
-    if (in_range(irq, msg.baseirq, 8) && msg.mask & (1 << (irq & 0x7)))
-      {
-	//Logging::printf("Notify irq message #%x  %x -> %x\n", msg.mask, msg.baseirq, _host_irqs);
-	MessageHostOp msg2(MessageHostOp::OP_NOTIFY_IRQ, _host_irqs[0]);
-	return _mb.bus_hostop.send(msg2);
-      }
+    if (in_range(irq, msg.baseirq, 8) && msg.mask & (1 << (irq & 0x7))) {
+      //Logging::printf("Notify irq message #%x  %x -> %x\n", msg.mask, msg.baseirq, _host_irqs);
+      MessageHostOp msg2(MessageHostOp::OP_NOTIFY_IRQ, _host_irqs[0]);
+      return _mb.bus_hostop.send(msg2);
+    }
     return false;
   }
 
@@ -221,35 +249,38 @@ class DirectPciDevice : public StaticReceiver<DirectPciDevice>, public HostPci
   bool receive(MessageMemAlloc &msg)
   {
     COUNTER_INC("PCIDirect::alloc");
-    unsigned addr = msg.phys1 & ~0xfff;
-    unsigned ofs = match_bars(addr, 0x1000, false);
-    if (!ofs || msg.phys2 != ~0xffful)  return false;
+    char *ptr;
+    unsigned ofs = match_bars(msg.phys1 & ~0xfff, 0x1000, ptr);
     unsigned bar = _cfgspace[ofs];
-    // prefetchable?
-    if (~bar & 0x8) return false;
-    *msg.ptr = reinterpret_cast<void *>(addr + (msg.phys1 & 0xfff));
+    // there is a bar and it does not cross multiple pages and it is prefetchable?
+    if (!ofs || msg.phys2 != ~0xffful || ~bar & 0x8)  return false;
+    *msg.ptr = ptr + (msg.phys1 & 0xfff);
     return true;
 
   }
 
   bool receive(MessageMemWrite &msg)
   {
-    unsigned addr = msg.phys;
-    if (!match_bars(addr, msg.count, false))  return false;
+
+    char *ptr;
+    unsigned ofs = match_bars(msg.phys, msg.count, ptr);
+    if (!ofs)  return false;
     COUNTER_INC("PCIDirect::write");
-    switch (msg.count)
-      {
-      case 4:
-	*reinterpret_cast<unsigned       *>(addr) = *reinterpret_cast<unsigned       *>(msg.ptr);
-	break;
+    switch (msg.count) {
+    case 4:
+      *reinterpret_cast<unsigned       *>(ptr) = *reinterpret_cast<unsigned       *>(msg.ptr);
+       // write msix control trough
+       if (_msix_host_table && ptr >= reinterpret_cast<char *>(_msix_table) && ptr < reinterpret_cast<char *>(_msix_table + _irq_count) && (msg.phys & 0xf) == 0xc)
+	_msix_host_table[(ptr - reinterpret_cast<char *>(_msix_table)) / 16].control = *reinterpret_cast<unsigned *>(msg.ptr);
+      break;
       case 2:
-	*reinterpret_cast<unsigned short *>(addr) = *reinterpret_cast<unsigned short *>(msg.ptr);
+	*reinterpret_cast<unsigned short *>(ptr) = *reinterpret_cast<unsigned short *>(msg.ptr);
 	break;
       case 1:
-	*reinterpret_cast<unsigned char  *>(addr) = *reinterpret_cast<unsigned char  *>(msg.ptr);
+	*reinterpret_cast<unsigned char  *>(ptr) = *reinterpret_cast<unsigned char  *>(msg.ptr);
 	break;
       default:
-	memcpy(reinterpret_cast<void *>(addr), msg.ptr, msg.count);
+	memcpy(ptr, msg.ptr, msg.count);
       }
     return true;
   }
@@ -257,56 +288,55 @@ class DirectPciDevice : public StaticReceiver<DirectPciDevice>, public HostPci
 
   bool receive(MessageMemRead &msg)
   {
-    unsigned addr = msg.phys;
-    if (!match_bars(addr, msg.count, false))  return false;
+    char *ptr;
+    if (!match_bars(msg.phys, msg.count, ptr))  return false;
+
     COUNTER_INC("PCIDirect::read");
     switch (msg.count)
       {
       case 4:
-	*reinterpret_cast<unsigned       *>(msg.ptr) = *reinterpret_cast<unsigned       *>(addr);
+	*reinterpret_cast<unsigned       *>(msg.ptr) = *reinterpret_cast<unsigned       *>(ptr);
 	break;
       case 2:
-	*reinterpret_cast<unsigned short *>(msg.ptr) = *reinterpret_cast<unsigned short *>(addr);
+	*reinterpret_cast<unsigned short *>(msg.ptr) = *reinterpret_cast<unsigned short *>(ptr);
 	break;
       case 1:
-	*reinterpret_cast<unsigned char  *>(msg.ptr) = *reinterpret_cast<unsigned char  *>(addr);
+	*reinterpret_cast<unsigned char  *>(msg.ptr) = *reinterpret_cast<unsigned char  *>(ptr);
 	break;
       default:
-	memcpy(msg.ptr, reinterpret_cast<void *>(addr), msg.count);
+	memcpy(msg.ptr, ptr, msg.count);
       }
     return true;
   }
 
-  bool  receive(MessageMemMap &msg)
-  {
-    for (unsigned i=0; i < _bar_count; i++)
-      {
-	unsigned  bar = _cfgspace[4 + i];
-	if (_sizes[i] || (bar & 1) || !in_range(msg.phys, bar & ~0x3, _sizes[i]))
-	  continue;
-	msg.ptr  = reinterpret_cast<char *>(_bases[i] & ~0xfff);
-	msg.count = _sizes[i];
-	Logging::printf(" MAP %lx+%x from %p\n", msg.phys, msg.count, msg.ptr);
-	return true;
-      }
-    return false;
+  bool  receive(MessageMemMap &msg) {
+    char *ptr;
+    unsigned ofs = match_bars(msg.phys & ~0xfff, 0x1000, ptr);
+    if (!ofs) return false;
+
+    if (_msix_host_table && ptr >= reinterpret_cast<char *>(_msix_table) && ptr < reinterpret_cast<char *>(_msix_table + _irq_count))
+      return false;
+
+    // XXX support large pages
+    msg.ptr = ptr;
+    msg.count = 0x1000;
+    Logging::printf(" MAP %lx+%x from %p\n", msg.phys, msg.count, msg.ptr);
+    return true;
   }
 
   DirectPciDevice(Motherboard &mb, unsigned bdf, unsigned dstbdf, unsigned long long *bases, unsigned long long *sizes, unsigned didvid = 0)
-    : HostPci(mb.bus_hwpcicfg, mb.bus_hostop), _mb(mb), _bdf(bdf), _msix_table(0), _bar_count(count_bars(_bdf))
+    : HostPci(mb.bus_hwpcicfg, mb.bus_hostop), _mb(mb), _bdf(bdf), _msix_table(0), _msix_host_table(0), _bar_count(count_bars(_bdf))
   {
 
-    memcpy(_bases, bases, sizeof(_bases));
-    memcpy(_sizes, sizes, sizeof(_sizes));
-
     for (unsigned i=0; i < PCI_CFG_SPACE_DWORDS; i++) _cfgspace[i] = conf_read(_bdf, i<<2);
+    map_bars(bases, sizes);
 
 
     _msi_cap  = find_cap(bdf, CAP_MSI);
-    _msix_cap = find_cap(bdf, CAP_MSIX);
     _msi_64bit = false;
+    _msix_cap = find_cap(bdf, CAP_MSIX);
+    _msix_bar = ~0;
     _irq_count = 1;
-
     if (_msi_cap)  {
       _irq_count = 1 << ((_cfgspace[_msi_cap/4] >> 1) & 0x7);
       _msi_64bit = _cfgspace[_msi_cap/4] & 0x800000;
@@ -315,11 +345,14 @@ class DirectPciDevice : public StaticReceiver<DirectPciDevice>, public HostPci
       unsigned msix_irqs = 1 + ((_cfgspace[_msix_cap/4] >> 16) & 0x7ff);
       if (_irq_count < msix_irqs) _irq_count = msix_irqs;
       _msix_table = new MsiXTableEntry[_irq_count];
+      _msix_bar  = _cfgspace[1 + _msix_cap/4] & 0x7;
+      _msix_host_table = reinterpret_cast<MsiXTableEntry *>(_barinfo[_msix_bar].ptr + (_cfgspace[1 + _msix_cap/4] & ~0x7));
     }
 
     _host_irqs = new unsigned[_irq_count];
     for (unsigned i=0; i < _irq_count; i++)
-      _host_irqs[i] = get_gsi(mb.bus_hostop, mb.bus_acpi, bdf, i, true);
+      // XXX when do we need level?
+      _host_irqs[i] = get_gsi(mb.bus_hostop, mb.bus_acpi, bdf, i, i ? false : true);
 
     dstbdf = (dstbdf == 0) ? bdf : PciHelper::find_free_bdf(mb.bus_pcicfg, dstbdf);
     mb.bus_pcicfg.add(this, &DirectPciDevice::receive_static<MessagePciConfig>, dstbdf);
