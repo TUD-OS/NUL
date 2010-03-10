aa5763851a48262fbfa7c1b74df75a35bbd34a54
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-03-10 15:30:54 +0100

    optimization

--- vancouver/model/reg.h
+++ vancouver/model/reg.h
@@ -1,6 +1,6 @@
 #define REG_RO(NAME, OFFSET, VALUE) REG(NAME, OFFSET, static const unsigned NAME = VALUE;, value = VALUE; , break; , )
-#define REG_RW(NAME, OFFSET, VALUE, MASK) REG(NAME, OFFSET, unsigned NAME;, value = NAME; , NAME = (NAME & ~MASK) | (value & MASK); , NAME=VALUE;)
-#define REG_WR(NAME, OFFSET, VALUE, MASK, RW1S, RW1C, WRITE_CALLBACK) REG(NAME, OFFSET, unsigned NAME; , value = NAME; , unsigned oldvalue = NAME; value = value & ~RW1S | ( value | oldvalue) & RW1S; value = value & ~RW1C | (~value & oldvalue) & RW1C; NAME = (NAME & ~MASK) | (value & MASK); WRITE_CALLBACK , NAME = VALUE;)
+#define REG_RW(NAME, OFFSET, VALUE, MASK) REG(NAME, OFFSET, unsigned NAME;, value = NAME; , if (!MASK) return false; NAME = (NAME & ~MASK) | (value & MASK); , NAME=VALUE;)
+#define REG_WR(NAME, OFFSET, VALUE, MASK, RW1S, RW1C, WRITE_CALLBACK) REG(NAME, OFFSET, unsigned NAME; , value = NAME; ,  if (!MASK) return false; unsigned oldvalue = NAME; value = value & ~RW1S | ( value | oldvalue) & RW1S; value = value & ~RW1C | (~value & oldvalue) & RW1C; NAME = (NAME & ~MASK) | (value & MASK); WRITE_CALLBACK , NAME = VALUE;)
 #define REGSET(NAME, ...) private: __VA_ARGS__
 #define REG(NAME, OFFSET, MEMBER, READ, WRITE, RESET) MEMBER
 #include REGBASE
