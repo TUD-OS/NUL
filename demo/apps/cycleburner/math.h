a5cda549596d079e0d522074fc1baf441c2233b5
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-03-14 17:58:29 +0100

    let cycleburner compile without crun

--- demo/apps/cycleburner/math.h
+++ demo/apps/cycleburner/math.h
@@ -2,7 +2,13 @@
 
 #pragma once
 
-#include "cstdint"
+#include "nul/types.h"
+
+typedef signed char  int8_t;
+typedef signed short int16_t;
+typedef uint8  uint8_t;
+typedef uint16 uint16_t;
+typedef uint32 uint32_t;
 
 # define M_PI		3.14159265358979323846
 
