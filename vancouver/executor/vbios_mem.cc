6c2938aa50bd91c2896903c9c5fa996c8b644851
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-03-16 16:00:27 +0100

    finish split of vbios

---
+++ vancouver/executor/vbios_mem.cc
@@ -0,0 +1,146 @@
+/**
+ * Virtual Bios memory routines.
+ *
+ * Copyright (C) 2009-2010, Bernhard Kauer <bk@vmmon.org>
+ *
+ * This file is part of Vancouver.
+ *
+ * Vancouver is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Vancouver is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
+#include "nul/motherboard.h"
+#include "executor/bios.h"
+
+/**
+ * Virtual Bios mem routines.
+ * Features: int
+ * Missing:
+ */
+class VirtualBiosMem : public StaticReceiver<VirtualBiosMem>, public BiosCommon
+{
+  unsigned long _memsize;
+
+
+  /**
+   * Memory+PS2 INT.
+   */
+  bool handle_int15(CpuState *cpu)
+  {
+    COUNTER_INC("int15");
+    // default is to clear CF
+    cpu->efl &= ~1;
+    switch (cpu->ax)
+      {
+      case 0x2400: // disable A20
+      case 0x2401: // enable  A20
+	{
+	  MessageLegacy msg(MessageLegacy::GATE_A20, cpu->al);
+	  if (_mb.bus_legacy.send(msg))
+	    cpu->ax = 0;
+	  else
+	    error(cpu, 0x24);
+	}
+	break;
+      case 0xc201:            // reset mouse
+	{
+	  cpu->ax = 0x0001;
+	  cpu->bx = 0xfaaa;
+	}
+	break;
+      case 0xe820:           // get memory map
+	{
+	  if ((cpu->edx == 0x534D4150 && cpu->ecx >= 20))
+	    {
+	      Logging::printf("%s() ebx %x sizeof(mymap) %x rdi %x memsize %lx\n", __func__, cpu->ebx, 2, cpu->edi, _memsize);
+	      struct mmap{
+		unsigned long long base;
+		unsigned long long size;
+		unsigned type;
+	      } mmap;
+	      mmap.type = 1;
+	      switch (cpu->ebx)
+		{
+		case 0:
+		  mmap.base = 0;
+		  mmap.size = 0xa0000;
+		  cpu->ebx++;
+		  break;
+		case 1:
+		  mmap.base = 1<<20;
+		  mmap.size = _memsize - (1<<20);
+		  cpu->ebx = 0;
+		  break;
+		default:
+		  mmap.type = 0;
+		  break;
+		}
+
+	      if (mmap.type)
+		{
+		  copy_out(cpu->es.base + cpu->di, &mmap, 20);
+		  cpu->eax = cpu->edx;
+		  cpu->ecx = 20;
+		  cpu->edx = 0;
+		  break;
+		}
+	    }
+	  goto unsupported;
+	}
+      case 0x00c0:            // get rom configtable
+      case 0x5300 ... 0x53ff: // apm installation check
+      case 0x8800 ... 0x88ff: // get extended memory
+      case 0xc000 ... 0xc0ff: // get configuration
+      case 0xc100 ... 0xc1ff: // get ebda
+      case 0xe801:            // get memsize
+      case 0xe980:            // get intel speedstep information
+      unsupported:
+	// unsupported
+	DEBUG;
+	error(cpu, 0x86);
+	break;
+      default:
+	VB_UNIMPLEMENTED;
+      }
+    return true;
+  }
+
+public:
+
+  bool  receive(MessageBios &msg) {
+    switch(msg.irq) {
+    case 0x0f:  return true;
+    case 0x10:  return true;
+    case 0x11: // BIOS equipment word
+      msg.cpu->ax = 0x34; // 80x25, ps2-mouse, no-floppy
+      return true;
+    case 0x12: // get low memory
+      msg.cpu->ax = read_bda(0x13);
+      return true;
+    case 0x15:  return handle_int15(msg.cpu);
+    case 0x17:  // printer
+      msg.cpu->efl |= 1;  // unsupported
+      return true;
+    default:    return false;
+    }
+  }
+
+  VirtualBiosMem(Motherboard &mb) : BiosCommon(mb) {
+    MessageHostOp msg1(MessageHostOp::OP_GUEST_MEM, 0);
+    if (!mb.bus_hostop.send(msg1))
+      Logging::panic("%s can't get physical memory size", __PRETTY_FUNCTION__);
+    _memsize = msg1.len;
+  }
+};
+
+PARAM(vbios_mem,
+      mb.bus_bios.add(new VirtualBiosMem(mb), &VirtualBiosMem::receive_static<MessageBios>);
+      ,
+      "vbios_mem - provide memory related virtual BIOS functions.");
+
---
+++ vancouver/executor/vbios_reset.cc
@@ -0,0 +1,107 @@
+/**
+ * Virtual Bios reset routines.
+ *
+ * Copyright (C) 2009-2010, Bernhard Kauer <bk@vmmon.org>
+ *
+ * This file is part of Vancouver.
+ *
+ * Vancouver is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Vancouver is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
+#include "nul/motherboard.h"
+#include "executor/bios.h"
+
+/**
+ * Virtual Bios reset routines.
+ * Features: init of PIC, PIT
+ * Missing: serial port, ACPI tables
+ */
+class VirtualBiosReset : public StaticReceiver<VirtualBiosReset>, public BiosCommon
+{
+  /**
+   * Out to IO-port.
+   */
+  void outb(unsigned short port, unsigned value)
+  {
+    MessageIOOut msg(MessageIOOut::TYPE_OUTB, port, value);
+    _mb.bus_ioout.send(msg);
+  }
+
+  /**
+   * called on reset.
+   */
+  void reset_helper()
+  {
+
+    // initialize PIT0
+    // let counter0 count with minimal freq of 18.2hz
+    outb(0x40+3, 0x24);
+    outb(0x40+0, 0);
+
+    // let counter1 generate 15usec refresh cycles
+    outb(0x40+3, 0x56);
+    outb(0x40+1, 0x12);
+
+
+    // the master PIC
+    // ICW1-4+IMR
+    outb(0x20+0, 0x11);
+    outb(0x20+1, 0x08); // offset 0x08
+    outb(0x20+1, 0x04); // has slave on 2
+    outb(0x20+1, 0x0f); // is buffer, master, AEOI and x86
+    outb(0x20+1, 0xfc);
+
+    // the slave PIC + IMR
+    outb(0xa0+0, 0x11);
+    outb(0xa0+1, 0x70); // offset 0x70
+    outb(0xa0+1, 0x02); // is slave on 2
+    outb(0xa0+1, 0x0b); // is buffer, slave, AEOI and x86
+    outb(0xa0+1, 0xff);
+
+
+    // initilize bios data area
+    // we have 640k low memory
+    write_bda(0x13, 640, 2);
+    // keyboard buffer
+    write_bda(0x1a, 0x1e001e, 4);
+    write_bda(0x80, 0x2f001e, 4);
+
+
+#if 0
+    // XXX announce number of serial ports
+    // announce port
+    write_bios_data_area(mb, serial_count * 2 - 2,      argv[0]);
+    write_bios_data_area(mb, serial_count * 2 - 2 + 1,  argv[0] >> 8);
+
+    // announce number of serial ports
+    write_bios_data_area(mb, 0x11, read_bios_data_area(mb, 0x11) & ~0xf1 | (serial_count*2));
+#endif
+  };
+
+
+public:
+
+  bool  receive(MessageBios &msg) {
+    switch(msg.irq) {
+    case RESET_VECTOR:
+      reset_helper();
+      return jmp_int(msg.cpu, 0x19);
+    default:    return false;
+    }
+  }
+
+  VirtualBiosReset(Motherboard &mb) : BiosCommon(mb) {}
+};
+
+PARAM(vbios_reset,
+      mb.bus_bios.add(new VirtualBiosReset(mb), &VirtualBiosReset::receive_static<MessageBios>);
+      ,
+      "vbios_reset - provide reset handling for virtual BIOS functions.");
+
