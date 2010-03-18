d24c508c0a7b4d8ac794a493f90ad87ab68a70c2
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2010-03-18 15:42:09 +0100

    Simplify rx queues.

--- julian/model/82576vf.cc
+++ julian/model/82576vf.cc
@@ -6,10 +6,10 @@
 #include <model/pci.h>
 
 /// NOTES
-// 
+//
 // We emulate more bits in the PCI COMMAND register than the real VF
 // supports.
-// 
+//
 // We can probably map the receive register range 0x2000 - 0x2FFF
 // directly into VM space, if we ignore RXDCTL.SWFLUSH (Software
 // Flush).
@@ -19,6 +19,8 @@ class Model82576vf : public StaticReceiver<Model82576vf>
   uint64 _mac;
   DBus<MessageNetwork>  &_net;
   DBus<MessageIrq>      &_irqlines;
+  DBus<MessageMemWrite> &_memwrite;
+  DBus<MessageMemRead>  &_memread;
 
   uint32 _mem_mmio;
   uint32 _mem_msix;
@@ -26,14 +28,9 @@ class Model82576vf : public StaticReceiver<Model82576vf>
 #include <model/82576vfmmio.inc>
 #include <model/82576vfpci.inc>
 
-  struct Queue {
+  struct rx_queue {
+    Model82576vf &parent;
     unsigned n;
-
-    // Too bad we have to duplicate these per queue, but my C++ skills
-    // are simply to limited to think of a better solution that does
-    // not look like someone justed barfed in my Emacs window...
-    DBus<MessageMemWrite> &memwrite;
-    DBus<MessageMemRead>  &memread;
 
     uint32 &rxdctl;
     uint32 &rdbal;
@@ -83,14 +80,14 @@ class Model82576vf : public StaticReceiver<Model82576vf>
 	Logging::printf("Dropped packet: Queue %u has no receive descriptors available.\n", n);
 	return;
       }
-      
+
       // assert(rdbah == 0);
       uint64 addr = (static_cast<uint64>(rdbah)<<32 | rdbal) + ((rdh*16) % rdlen);
       rx_desc desc;
-      
+
       Logging::printf("RX descriptor at %llx\n", addr);
       MessageMemRead msg(addr, desc.raw, sizeof(desc));
-      if (!memread.send(msg)) {
+      if (!parent._memread.send(msg)) {
 	Logging::printf("RX descriptor fetch failed.\n");
 	return;
       }
@@ -102,7 +99,7 @@ class Model82576vf : public StaticReceiver<Model82576vf>
 	{
 	  MessageMemWrite m(desc.legacy.buffer, buf, size);
 	  desc.legacy.status = 0;
-	  if (!memwrite.send(m))
+	  if (!parent._memwrite.send(m))
 	    desc.legacy.status |= 0x8000; // RX error
 	  desc.legacy.status |= 0x3; // EOP, DD
 	  desc.legacy.sumlen = size;
@@ -114,13 +111,13 @@ class Model82576vf : public StaticReceiver<Model82576vf>
       }
 
       MessageMemWrite m(addr, desc.raw, sizeof(desc));
-      if (!memwrite.send(m))
+      if (!parent._memwrite.send(m))
 	Logging::printf("RX descriptor store failed.\n");
 
       // Advance queue head
       rdh = (((rdh+1)*16 ) % rdlen) / 16;
 
-      // XXX IRQ!
+      parent.RX_irq(n);
     }
   } _rx_queues[2];
 
@@ -156,6 +153,8 @@ class Model82576vf : public StaticReceiver<Model82576vf>
   // Generate a MSI-X IRQ.
   void MSIX_irq(unsigned nr)
   {
+    Logging::printf("MSI-X IRQ %d | EIMS %02x | C %02x\n", nr,
+		    rVTEIMS, _msix.table[nr].vector_control);
     uint32 mask = 1<<nr;
     // Set interrupt cause.
     rVTEICR |= mask;
@@ -182,6 +181,22 @@ class Model82576vf : public StaticReceiver<Model82576vf>
       MSIX_irq(rVTIVAR_MISC & 0x3);
   }
 
+  void RX_irq(unsigned nr)
+  {
+    uint32 va = rVTIVAR >> (nr*16);
+    if ((va & 0x80) != 0)
+      MSIX_irq(va & 0x3);
+    else
+      Logging::printf("RX irq %u is disabled.\n", nr);
+  }
+
+  void TX_irq(unsigned nr)
+  {
+    uint32 va = rVTIVAR >> (nr*16 + 8);
+    if ((va & 0x80) != 0)
+      MSIX_irq(va & 0x3);
+  }
+
   void VMMB_cb(uint32 old, uint32 val)
   {
     // See 82576 datasheet Table 7-71 on page 357 for a good
@@ -204,7 +219,7 @@ class Model82576vf : public StaticReceiver<Model82576vf>
 	// Ignore
 	rVFMBX0 |= CMD_ACK | CTS;
 	break;
-	
+
       default:
 	Logging::printf("VF message unknown %08x\n", rVFMBX0 & 0xFFFF);
 	rVFMBX0 |= CMD_NACK;
@@ -223,7 +238,7 @@ class Model82576vf : public StaticReceiver<Model82576vf>
       // message.
       rVMMB &= (1<<3 /* PFU */);
     }
-     
+
     rVMMB &= ~3;
   }
 
@@ -305,7 +320,7 @@ public:
     } else if ((msg.phys & ~0xFFF) == (rPCIBAR3 & ~0xFFF)) {
       MSIX_write(msg.phys - (rPCIBAR3 & ~0xFFF), *(reinterpret_cast<uint32 *>(msg.ptr)));
     } else return false;
-    
+
     return true;
   }
 
@@ -317,17 +332,18 @@ public:
     return true;
   }
 
-  Model82576vf(uint64 mac, DBus<MessageNetwork> net, DBus<MessageIrq> irqlines, 
+  Model82576vf(uint64 mac, DBus<MessageNetwork> net, DBus<MessageIrq> irqlines,
 	       DBus<MessageMemWrite> &memwrite, DBus<MessageMemRead> &memread,
-	       uint32 mem_mmio, uint32 mem_msix) 
+	       uint32 mem_mmio, uint32 mem_msix)
     : _mac(mac), _net(net), _irqlines(irqlines),
+      _memwrite(memwrite), _memread(memread),
       _mem_mmio(mem_mmio), _mem_msix(mem_msix),
-      _rx_queues( {{ 0, memwrite, memread, rRXDCTL0, rRDBAL0, rRDBAH0, rRDT0, rRDH0, rRDLEN0, rSRRCTL0 },
-	           { 1, memwrite, memread, rRXDCTL1, rRDBAL1, rRDBAH1, rRDT1, rRDH1, rRDLEN1, rSRRCTL1 }})
+      _rx_queues( {{ *this, 0, rRXDCTL0, rRDBAL0, rRDBAH0, rRDT0, rRDH0, rRDLEN0, rSRRCTL0 },
+		   { *this, 1, rRXDCTL1, rRDBAL1, rRDBAH1, rRDT1, rRDH1, rRDLEN1, rSRRCTL1 }})
   {
     Logging::printf("Attached 82576VF model at %08x+0x4000, %08x+0x1000\n",
 		    mem_mmio, mem_msix);
-    
+
     PCI_init();
     rPCIBAR0 = _mem_mmio;
     rPCIBAR3 = _mem_msix;
@@ -364,6 +380,6 @@ PARAM(82576vf,
       "82576vf:mem_mmio,mem_msix - attach an Intel 82576VF to the PCI bus."
       "Example: 82576vf:0xf7ce0000,0xf7cc0000"
       );
-      
+
 
 // EOF
