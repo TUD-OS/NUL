9996ed734aab4a0fcfd62717851a55272774d91f
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2010-03-19 16:34:13 +0100

    Removed ununsed register definitions.

--- julian/model/82576vf/reg_mmio.py
+++ julian/model/82576vf/reg_mmio.py
@@ -28,6 +28,7 @@ rset = [
       'write-only' : True },
     # Interrupt Mask
     { 'name' : 'rVTEIMS',
+      'important' : 100,
       'offset' : 0x1524,
       'initial' : 0,
       'w1s' : True },
@@ -60,26 +61,5 @@ for n in range(0x10):
     rset.append({'name' : 'rVFMBX%d' % n,
                  'offset' : 0x800 + 4*n,
                  'initial' : 0})
-
-# Queues
-for n in range(2):
-    rset.append({'name' : 'rRDBAL%d' % n, 'offset' : 0x2800 + n*256,
-                 'initial' : 0, 'mutable' : ~0x3F})
-    rset.append({'name' : 'rRDBAH%d' % n, 'offset' : 0x2804 + n*256,
-                 'initial' : 0, 'mutable' : ~0x3F})
-    rset.append({'name' : 'rRDLEN%d' % n, 'offset' : 0x2808 + n*256,
-                 'initial' : 0, 'mutable' : 0xFFFFC0})
-    rset.append({'name' : 'rRDH%d' % n, 'offset' : 0x2810 + n*256,
-                 'initial' : 0, 'mutable' : 0xFFFF})
-    rset.append({'name' : 'rRDT%d' % n, 'offset' : 0x2818 + n*256,
-                 'initial' : 0, 'mutable' : 0xFFFF})
-    rset.append({'name' : 'rRXDCTL%d' % n, 'offset' : 0x2828 + n*256,
-                 'callback' : 'RXDCTL%d_cb' % n,
-                 'initial' : 1<<16 | ((1<<25) if n == 0 else 0),
-                 'mutable' : ~(1<<26) # SWFLUSH is WC
-                 })
-    rset.append({'name' : 'rSRRCTL%d' % n, 'offset' : 0x280C + n*256,
-                 'initial' : 0x400 | (0x80000000 if n != 0 else 0),
-                 'mutable' : 0xFFFF})
 
 # EOF
