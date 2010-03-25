dbbf9b0b9b49d03f94f48b6b6188deb6330f1699
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-03-25 23:11:09 +0100

    clear the whole screen

--- vancouver/model/vga.cc
+++ vancouver/model/vga.cc
@@ -72,7 +72,7 @@ class Vga : public StaticReceiver<Vga>, public BiosCommon
     _regs.cursor_style = 0x0d0e;
 
     // clear screen
-    for (unsigned i=0; i < 25; i++) putchar_guest('\n');
+    memset(_framebuffer_ptr, 0, _framebuffer_size);
     puts_guest("    VgaBios booting...\n\n\n");
     write_bda(0x4a, 80, 1); // columns on screen
     write_bda(0x84, 24, 1); // rows - 1
