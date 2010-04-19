49a80a00bb86a47cdb7b7646bf6b3590d2f884cb
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2010-04-19 15:32:42 +0200

    New host_op: Allocate semaphore.

--- vancouver/include/nul/message.h
+++ vancouver/include/nul/message.h
@@ -402,6 +402,7 @@ struct MessageHostOp
       OP_ATTACH_MSI,
       OP_ALLOC_IOIO_REGION,
       OP_ALLOC_IOMEM,
+      OP_ALLOC_SEM,
       OP_ASSIGN_PCI,
       OP_VIRT_TO_PHYS,
       OP_GET_MODULE,
