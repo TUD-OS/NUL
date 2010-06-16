7dadd9e47ff852b0aaab68ba780dc4261111bef8
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-06-17 01:33:08 +0200

    output early

--- vancouver/host/hostahci.cc
+++ vancouver/host/hostahci.cc
@@ -462,8 +462,8 @@ PARAM(hostahci,
 	  bool dmar = mb.bus_hostop.send(msg1);
 	  unsigned irqline = pci.get_gsi(mb.bus_hostop, mb.bus_acpi, bdf, 0);
 
+	  Logging::printf("DISK controller #%x AHCI %x id %x mmio %x\n", num, bdf, pci.conf_read(bdf, 0), pci.conf_read(bdf, 9));
 	  HostAhci *dev = new HostAhci(pci, mb.bus_hostop, mb.bus_disk, mb.bus_diskcommit, mb.clock(), bdf, irqline, dmar);
-	  Logging::printf("DISK controller #%x AHCI %x id %x\n", num, bdf, pci.conf_read(bdf, 0));
 	  mb.bus_hostirq.add(dev, &HostAhci::receive_static<MessageIrq>);
 
 	}
