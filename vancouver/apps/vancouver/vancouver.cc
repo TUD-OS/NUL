d2ce31068638a0061c99a4d8bc1eea84a395a054
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-06-17 01:36:14 +0200

    check for IRQs after every instruction

--- vancouver/apps/vancouver/vancouver.cc
+++ vancouver/apps/vancouver/vancouver.cc
@@ -377,6 +377,16 @@ class Vancouver : public NovaProgram, public ProgramConsole, public StaticReceiv
     }
 
     /**
+     * check whether we should inject something
+     */
+    if (msg.mtr_in & MTD_INJ) {
+      msg.type = CpuMessage::TYPE_CHECK_IRQ;
+      if (!vcpu->executor.send(msg, true))
+	Logging::panic("nobody to execute %s at %x:%x pid %d\n", __func__, utcb->cs.sel, utcb->eip, pid);
+    }
+
+
+    /**
      * If the IRQ injection is performed, recalc the IRQ window.
      */
     if (msg.mtr_out & MTD_INJ) {
