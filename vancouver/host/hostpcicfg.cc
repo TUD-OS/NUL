1e2d8f2864415731682f36dfebcd7b9eea8fee90
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-07-14 02:25:42 +0200

    cleanup pci config space access

--- vancouver/host/hostpcicfg.cc
+++ vancouver/host/hostpcicfg.cc
@@ -21,46 +21,40 @@
  * Provide HW PCI config space access by bridging PCI cfg read/write
  * messages to the HW IO busses.
  *
- * State: testing
+ * State: stable
  * Documentation: pci3 spec
  */
-class PciConfigAccess : public StaticReceiver<PciConfigAccess>
+struct PciConfigAccess : public StaticReceiver<PciConfigAccess>
 {
   static const unsigned BASE = 0xcf8;
   DBus<MessageIOIn>  &_hwioin;
   DBus<MessageIOOut> &_hwioout;
-public:
 
   PciConfigAccess(DBus<MessageIOIn> &hwioin, DBus<MessageIOOut> &hwioout) : _hwioin(hwioin), _hwioout(hwioout) {};
-  bool  receive(MessagePciConfig &msg)
-  {
-    //Logging::printf("PCI CFG %x %x %x\n", msg.bdf, msg.dword, msg.value);
-    if (msg.dword < 0x40) {
-      MessageIOOut msg1(MessageIOOut::TYPE_OUTL, BASE, 0x80000000 |  (msg.bdf << 8) | (msg.dword << 2));
-      if (!_hwioout.send(msg1, true)) return false;
+  bool  receive(MessagePciConfig &msg) {
 
-      if (msg.type == MessagePciConfig::TYPE_READ) {
+    if (msg.dword >= 0x40 || (msg.bdf >= 0x10000)) return false;
 
-	MessageIOIn msg2(MessageIOIn::TYPE_INL, BASE+4);
-	bool res = _hwioin.send(msg2, true);
-	msg.value = msg2.value;
-	//Logging::printf("PCI READ %x = %x\n", msg.dword, msg2.value);
-	return res;
-      }
-      else {
- 	//Logging::printf("PCI WRITE %x %x\n", msg.dword, msg.value);
-	MessageIOOut msg2(MessageIOOut::TYPE_OUTL, BASE+4, msg.value);
-	return _hwioout.send(msg2, true);
-      }
-    }
-    return false;
+    MessageIOOut msg1(MessageIOOut::TYPE_OUTL, BASE, 0x80000000 |  (msg.bdf << 8) | (msg.dword << 2));
+    if (!_hwioout.send(msg1, true)) return false;
+
+    MessageIOOut msg2(MessageIOOut::TYPE_OUTL, BASE+4, msg.value);
+    if (msg.type == MessagePciConfig::TYPE_WRITE) return _hwioout.send(msg2, true);
+
+    MessageIOIn msg3(MessageIOIn::TYPE_INL, BASE+4);
+    bool res = _hwioin.send(msg3, true);
+    msg.value = msg3.value;
+    return res;
   }
 };
 
 
 PARAM(pcicfg,
       {
+	MessageHostOp msg1(MessageHostOp::OP_ALLOC_IOIO_REGION,  PciConfigAccess::BASE | 3);
+	check0(!mb.bus_hostop.send(msg1), "%s could not allocate ioports %x+8\n", __PRETTY_FUNCTION__, PciConfigAccess::BASE);
+
 	Device *dev = new PciConfigAccess(mb.bus_hwioin, mb.bus_hwioout);
-	mb.bus_hwpcicfg.add(dev, &PciConfigAccess::receive_static<MessagePciConfig>);
+	mb.bus_hwpcicfg.add(dev, PciConfigAccess::receive_static<MessagePciConfig>);
       },
       "pcicfg - provide HW PCI config space access through IO ports 0xcf8/0xcfc.");
