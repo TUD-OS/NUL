1c90d4d4c9b75654741d239875f94c4b4bfd01dc
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-07-15 21:53:13 +0200

    fix output

--- vancouver/include/host/simplehwioout.h
+++ vancouver/include/host/simplehwioout.h
@@ -35,12 +35,12 @@ void outsw(void *ptr, unsigned count, unsigned short port)
 {
   MessageIOOut msg(MessageIOOut::TYPE_OUTW, port, count, ptr);
   if (!_bus_hwioout.send(msg, true))
-    Logging::panic("%s could not read from ioport %x\n", __PRETTY_FUNCTION__, port);
+    Logging::panic("%s could not send to ioport %x\n", __PRETTY_FUNCTION__, port);
 }
 
 void outsl(void *ptr, unsigned count, unsigned short port)
 {
   MessageIOOut msg(MessageIOOut::TYPE_OUTL, port, count, ptr);
   if (!_bus_hwioout.send(msg, true))
-    Logging::panic("%s could not read from ioport %x\n", __PRETTY_FUNCTION__, port);
+    Logging::panic("%s could not send to ioport %x\n", __PRETTY_FUNCTION__, port);
 }
