4b9a7d4bed3c8320ec65023e06004a8ba52aa34d
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2010-07-20 16:59:59 +0200

    Implemented scrolling for tutor.nul.

--- demo/apps/tutor/screen.py
+++ demo/apps/tutor/screen.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 import sys
+from StringIO import StringIO
 
-print("void print_%s(unsigned short *dst) {" % sys.argv[1])
+table = StringIO()
+table.write(" unsigned written = 0;\n")
+table.write(" switch (line) {\n")
+
 input = sys.stdin
 lpos = 0
+screen_lines = 0
+
 while True:
-    print ("// Line %d" % lpos)
     lpos += 1
     line = input.readline()
     if not line:
@@ -15,6 +20,11 @@ while True:
 	continue
     pos = 0
     attr = 0x07
+
+    screen_lines += 1
+    table.write("case %d:\n" % (screen_lines - 1))
+    if (screen_lines > 25):
+        table.write(" if (++written > 25) break;\n")
     for c in line:
 	if c == '[':
 	    attr = 0x0F
@@ -23,8 +33,18 @@ while True:
 	elif c in ['\n']:
 	    pass
 	else:
-	    print(" *(dst+%d) = 0x%x;" % (pos, (attr << 8) | ord(c)))
+	    table.write("  *(dst+%d) = 0x%x;\n" % (pos, (attr << 8) | ord(c)))
 	    pos += 1
-    print(" dst += %d;" % 80)
+    table.write("  memset(dst + %d, 0, 160 - 2*%d);\n" % (pos, pos))
+    table.write("  dst += %d;\n" % 80)
+table.write(" }\n")
+table.write("memset(dst, 0, (25 - written) * 2*80);\n");
 
+print("#include <service/string.h>")
+print("void print_%s(unsigned short *dst, unsigned &line) {" % sys.argv[1])
+
+lline = (screen_lines - 24) if (screen_lines > 24) else 0
+print(" if (line > %d) line = %d; " % ( lline, lline ))
+print(table.getvalue())
+table.close()
 print("}")
--- demo/apps/tutor/tutor.cc
+++ demo/apps/tutor/tutor.cc
@@ -1,19 +1,21 @@
 /// -*- Mode: C++ -*-
 /// Show a screenful of helpful tips.
 ///
-/// Copyright (C) 2009, Julian Stecklina
+/// Copyright (C) 2009-2010, Julian Stecklina
 //
 /// This file is part of NUL, the NOVA userland. See LICENSE for
 /// licensing details.
 
 #include <nul/program.h>
+#include <host/keyboard.h>
 #include <sigma0/console.h>
 
 
-void print_screen1(unsigned short *dst);
+void print_screen1(unsigned short *dst, unsigned &line);
 
 struct Tutor : public NovaProgram,
-	       public ProgramConsole
+	       public ProgramConsole,
+	       public GenericKeyboard
 {
   void __attribute__((noreturn)) run(Utcb *utcb, Hip *hip)
   {
@@ -21,10 +23,27 @@ struct Tutor : public NovaProgram,
 
     init(hip);
 
-    print_screen1(_console_data.screen_address);
+    // Get keyboard
+    Logging::printf("Getting keyboard\n");
+    StdinConsumer stdinconsumer(alloc_cap());
+    Sigma0Base::request_stdin(utcb, &stdinconsumer, stdinconsumer.sm());
 
-    block_forever();
+    unsigned line = 0;
 
+    while (1) {
+      print_screen1(_console_data.screen_address, line);
+
+      MessageInput *kmsg = stdinconsumer.get_buffer();
+      switch (kmsg->data & ~KBFLAG_NUM) {
+      case KBCODE_UP:    if (line > 0) line -= 1; break;
+      case KBCODE_DOWN:  line +=  1; break;
+      case KBCODE_SPACE: line += 24; break;
+      }
+
+      stdinconsumer.free_buffer();
+    }
+    
+    // Not reached.
   }
 };
 
