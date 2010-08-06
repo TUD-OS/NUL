184b1de82578fd7dcf5a2c83ba2640b177e67f87
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-08-06 14:39:31 +0200

    check hip version

--- vancouver/apps/sigma0/sigma0.cc
+++ vancouver/apps/sigma0/sigma0.cc
@@ -1473,6 +1473,8 @@ struct Sigma0 : public Sigma0Base, public NovaProgram, public StaticReceiver<Sig
   void  __attribute__((noreturn)) run(Utcb *utcb, Hip *hip)
   {
     unsigned res;
+    // XXX check this in the hypervisor
+    assert(hip->api_ver == 2);
     if ((res = preinit(utcb, hip)))              Logging::panic("init() failed with %x\n", res);
     Logging::printf("Sigma0.nova:  hip %p caps %x memsize %x\n", hip, hip->cfg_cap, hip->mem_size);
 
