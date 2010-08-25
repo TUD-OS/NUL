99c28adccbb8f6da893d5245ed334857ffba1153
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2010-08-25 11:42:38 +0200

    common error codes

---
+++ vancouver/include/nul/error.h
@@ -0,0 +1,26 @@
+/*
+ * Error codes.
+ *
+ * Copyright (C) 2010, Bernhard Kauer <bk@vmmon.org>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of Vancouver.
+ *
+ * Vancouver.nova is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * Vancouver.nova is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+#pragma once
+
+enum {
+  ENONE        = 0,
+  EPROTO       = 0x100,
+  EPERM,
+  ERETRY,
+  ERESOURCE,
+};
