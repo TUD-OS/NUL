d67dba7b0ed5b31b40db6ea5f942477d6f16cf30
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2011-01-28 15:28:50 +0100

    Use WARN_UNUSED for syscalls.

--- vancouver/include/sys/syscalls.h
+++ vancouver/include/sys/syscalls.h
@@ -16,8 +16,10 @@
  * General Public License version 2 for more details.
  */
 #pragma once
-#include "hip.h"
-#include "utcb.h"
+
+#include <nul/compiler.h>
+#include <sys/hip.h>
+#include <sys/utcb.h>
 
 enum
 {
@@ -91,7 +93,7 @@ static inline unsigned char nova_syscall(unsigned w0, unsigned w1, unsigned w2, 
 }
 
 
-inline unsigned char  nova_call(unsigned idx_pt)
+WARN_UNUSED inline unsigned char  nova_call(unsigned idx_pt)
 {  return nova_syscall1(idx_pt << 8 | NOVA_IPC_CALL); }
 
 inline unsigned char  nova_create_pd (unsigned idx_pd, Crd pt_crd, unsigned dstpd = NOVA_DEFAULT_PD_CAP)
@@ -106,16 +108,16 @@ inline unsigned char  nova_create_ec(unsigned idx_ec, void *utcb, void *esp, uns
 inline unsigned char  nova_create_sc (unsigned idx_sc, unsigned idx_ec, Qpd qpd, unsigned dstpd = NOVA_DEFAULT_PD_CAP)
 {  return nova_syscall(idx_sc << 8 | NOVA_CREATE_SC, dstpd, idx_ec, qpd.value(), 0); }
 
-inline unsigned char  nova_create_pt(unsigned idx_pt, unsigned idx_ec, unsigned long eip, unsigned mtd, unsigned dstpd = NOVA_DEFAULT_PD_CAP)
+WARN_UNUSED inline unsigned char  nova_create_pt(unsigned idx_pt, unsigned idx_ec, unsigned long eip, unsigned mtd, unsigned dstpd = NOVA_DEFAULT_PD_CAP)
 {  return nova_syscall(idx_pt << 8 | NOVA_CREATE_PT, dstpd, idx_ec, mtd, eip); }
 
-inline unsigned char  nova_create_sm(unsigned idx_sm, unsigned initial=0, unsigned dstpd = NOVA_DEFAULT_PD_CAP)
+WARN_UNUSED inline unsigned char  nova_create_sm(unsigned idx_sm, unsigned initial=0, unsigned dstpd = NOVA_DEFAULT_PD_CAP)
 {  return nova_syscall(idx_sm << 8 | NOVA_CREATE_SM, dstpd, initial, 0, 0); }
 
-inline unsigned char  nova_revoke(Crd crd, bool myself)
+WARN_UNUSED inline unsigned char  nova_revoke(Crd crd, bool myself)
 {  return nova_syscall(myself ? NOVA_REVOKE_MYSELF : NOVA_REVOKE, crd.value(), 0, 0, 0); }
 
-inline Crd nova_lookup(void *address)
+WARN_UNUSED inline Crd nova_lookup(void *address)
 {
   unsigned res;
   Crd crd(reinterpret_cast<unsigned long>(address) >> 12, 32-12, DESC_MEM_ALL);
@@ -124,28 +126,30 @@ inline Crd nova_lookup(void *address)
   return Crd(res);
 }
 
-inline unsigned char  nova_recall(unsigned idx_ec)
+WARN_UNUSED inline unsigned char  nova_recall(unsigned idx_ec)
 {  return nova_syscall1(idx_ec << 8 | NOVA_RECALL); }
 
 
-inline unsigned char  nova_semup(unsigned idx_sm)
+WARN_UNUSED inline unsigned char  nova_semup(unsigned idx_sm)
 {  return nova_syscall1(idx_sm << 8 | NOVA_SEMCTL_UP); }
 
 
-inline unsigned char  nova_semdown(unsigned idx_sm)
+WARN_UNUSED inline unsigned char  nova_semdown(unsigned idx_sm)
 {  return nova_syscall1(idx_sm << 8 | NOVA_SEMCTL_DOWN); }
 
-inline unsigned char  nova_semdownmulti(unsigned idx_sm)
+WARN_UNUSED inline unsigned char  nova_semdownmulti(unsigned idx_sm)
 {  return nova_syscall1(idx_sm << 8 | NOVA_SEMCTL_DOWN_MULTI); }
 
-inline unsigned char  nova_assign_pci(unsigned pd, unsigned pf_rid, unsigned vf_rid)
+WARN_UNUSED inline unsigned char  nova_assign_pci(unsigned pd, unsigned pf_rid, unsigned vf_rid)
 {  return nova_syscall(pd << 8 | NOVA_ASSIGN_PCI, pf_rid, vf_rid, 0, 0); }
 
 
-inline unsigned char  nova_assign_gsi(unsigned idx_sm, unsigned cpu_nr, unsigned rid=0, unsigned long long* msi_address=0, unsigned *msi_value = 0)
+WARN_UNUSED inline unsigned char  nova_assign_gsi(unsigned idx_sm, unsigned cpu_nr, unsigned rid=0, unsigned long long* msi_address=0, unsigned *msi_value = 0)
 {
   unsigned out1;
   unsigned char res = nova_syscall(idx_sm << 8 | NOVA_ASSIGN_GSI, cpu_nr, rid, 0, 0, &out1, msi_value);
   if (msi_address) *msi_address = out1;
   return res;
 }
+
+// EOF
