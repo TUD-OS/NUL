ca33ce0b2b06fe7917118d6a24f25cc3c4ab7e68
Julian Stecklina <js@alien8.de>
2011-02-14 19:08:00 +0100

    Even more obsolete headers...

--- tools/release-check.sh
+++ tools/release-check.sh
@@ -14,11 +14,12 @@ echo  ... done
 
 echo
 echo Checking for files with obsolete license plates ...
-find . \( -name "*.[chS]" -or -name "*.cc" \) -exec grep -E -q "(is part of Vancouver|Vancouver.*is free sofware)" {} \; -print
+find . \( -name "*.[chS]" -or -name "*.cc" \) -exec grep -E -q "(is part of Vancouver|Vancouver.*is free sofware|Vancouver.*is distributed)" {} \; -print
 echo  ... done
 
 echo
 echo Checking for temporary files ...
 find . -name "*~"
+echo  ... done
 
 # EOF
