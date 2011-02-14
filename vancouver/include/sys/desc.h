28b3cbca077f3b68f7b8edc45fd14735feb381b1
Julian Stecklina <js@alien8.de>
2011-02-14 19:11:37 +0100

    System call headers license plates.

--- vancouver/include/sys/desc.h
+++ vancouver/include/sys/desc.h
@@ -1,21 +1,22 @@
 /*
- * Type Definitions
+ * Type definitions
  *
  * Copyright (C) 2008, Udo Steinberg <udo@hypervisor.org>
- * Changed for Vancouver: 2008, Bernhard Kauer <bk@vmmon.org>
+ * Copyright (C) 2008-2010, Bernhard Kauer <bk@vmmon.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
- * This file is part of the NOVA microhypervisor.
+ * This file is part of NUL.
  *
- * NOVA is free software: you can redistribute it and/or modify it
+ * NUL is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  *
- * NOVA is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License version 2 for more details.
+ * NUL is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License version 2 for more details.
  */
+
 
 
 #pragma once
