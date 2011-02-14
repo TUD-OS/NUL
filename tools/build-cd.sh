b666c9c9654b03267830199f23ebb2d4d4a77d4f
Julian Stecklina <js@alien8.de>
2011-02-14 18:52:16 +0100

    Updated ISO image building script. Builds hybrid disk/CD images by default now.

--- tools/build-cd.sh
+++ tools/build-cd.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 
-NUL_URL=os:/srv/git/repos/nul.git
-
-co_git() {
-    if [ ! -d $1/$3 ]; then
-	( cd "$1" && git clone "$2" "$3" ) || exit 1
-	( cd "$1/$3" && git checkout $4 ) || exit 1
-    else
-	( cd "$1/$3" && git pull ) || exit 1
-    fi
-}
-
-tftp_get() {
-    atftp --get -r "$2" -l "$3" "$1" || exit 4
-}
+BINARIES=$HOME/boot
 
 if [ -z "$1" ]; then
     TMPDIR=`mktemp -d`
@@ -23,49 +10,56 @@ fi
 
 echo "Working in $TMPDIR ..."; sleep 1
 
-co_git $TMPDIR $NUL_URL   nul   master
-
 ISO_DIR=$TMPDIR/iso
-rm -fdR $ISO_DIR
+rm -fr $ISO_DIR
 
-scons -C "$TMPDIR/nul/build"  target_cc=$HOME/local-install/bin/gcc target_cxx=$HOME/local-install/bin/g++ tftp=$ISO_DIR \
-    $ISO_DIR/nul/hypervisor \
-    $ISO_DIR/nul/sigma0.nul.gz \
-    $ISO_DIR/nul/vancouver.nul.gz \
-    $ISO_DIR/tools/santamonica \
-    $ISO_DIR/tools/munich  \
-    $ISO_DIR/tools/bender  \
- || exit 2
+mkdir -p $ISO_DIR/nul
+mkdir -p $ISO_DIR/tools
+
+cp -v $BINARIES/nul/hypervisor $ISO_DIR/nul/
+cp -v $BINARIES/nul/{sigma0,vancouver,rocknshine,tutor,cycleburner}.nul.gz $ISO_DIR/nul/
+cp -v $BINARIES/tools/{unzip,bender.gz,munich} $ISO_DIR/tools/
+cp -rv $BINARIES/pistachio $ISO_DIR/
+cp -rv $BINARIES/fiasco    $ISO_DIR/
 
 # Syslinux
 
-SYSLINUX_VER=4.03
-SYSLINUX_URL=http://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-${SYSLINUX_VER}.tar.bz2
-SYSLINUX_DIR=$TMPDIR/syslinux-${SYSLINUX_VER}
-
-if [ ! -d $SYSLINUX_DIR ]; then
-    ( cd $TMPDIR ; wget -O - "$SYSLINUX_URL" | tar xjf - ) || exit 3
-fi
-
-( cd $SYSLINUX_DIR ; make )
+SYSLINUX_DIR=/usr/share/syslinux
 
 mkdir -p $ISO_DIR/isolinux
-cp $SYSLINUX_DIR/core/isolinux.bin   $ISO_DIR/isolinux
+cp $SYSLINUX_DIR/isolinux.bin   $ISO_DIR/isolinux
 #cp $SYSLINUX_DIR/com32/modules/*.c32 $ISO_DIR/isolinux
-cp $SYSLINUX_DIR/com32/menu/menu.c32 $ISO_DIR/isolinux
-cp $SYSLINUX_DIR/com32/mboot/mboot.c32 $ISO_DIR/isolinux
+cp $SYSLINUX_DIR/menu.c32 $ISO_DIR/isolinux
+cp $SYSLINUX_DIR/mboot.c32 $ISO_DIR/isolinux
 
 # Scenarios
 
-mkdir -p $ISO_DIR/demo/linux || exit 4
+mkdir -p $ISO_DIR/linux || exit 4
 
-cp /srv/tftp/linux/bzImage-js $ISO_DIR/demo/linux/bzImage || exit 4
-cp /srv/tftp/linux/initrd-js.lzma $ISO_DIR/demo/linux/initrd || exit 4
+cat $BINARIES/tutor.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' > $ISO_DIR/tutor.nulconfig || exit 4
+cat $BINARIES/rocknshine.nulconfig | sed 's,rom://nul/r,rom:///nul/r,' | sed 's,rom://nul/e,rom:///nul/e,' | sed 's/\.nul\.gz/_nul.gz/' > $ISO_DIR/rocknshine.nulconfig || exit 4
+cat $BINARIES/cycleburner.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' > $ISO_DIR/cycleburner.nulconfig || exit 4
+cat $BINARIES/grml.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' > $ISO_DIR/grml.nulconfig || exit 4
+cat $BINARIES/linux.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' | sed 's/initrd-udo/initrd/' > $ISO_DIR/linux.nulconfig || exit 4
+cat $ISO_DIR/linux.nulconfig | sed 's/bzImage\.x/bzImage/' > $ISO_DIR/linux_novesa.nulconfig
+cat $BINARIES/pistachio/pistachio.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' | sed 's/i686-kernel/i686_kernel/' > $ISO_DIR/pistachio/pistachio.nulconfig
+cat $BINARIES/fiasco/fiasco.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' > $ISO_DIR/fiasco/fiasco.nulconfig
+
+
+cp $BINARIES/linux/grml.iso $ISO_DIR/linux/ || exit 4
+cp $BINARIES/nul/eurosys.slides $ISO_DIR/nul/ || exit 4
+cp $BINARIES/linux/bzImage.x $ISO_DIR/linux/bzImage.x || exit 4
+cp $BINARIES/linux/bzImage $ISO_DIR/linux/bzImage || exit 4
+cp $BINARIES/linux/initrd-udo $ISO_DIR/linux/initrd || exit 4
 
 # ISOLINUX config
 
-SARGS="S0_DEFAULT hostvga:0x40000,0x40000 hostkeyb:0,0x60,1,12 hostserial tracebuffer:32768 script script_start:1 quota::guid namespace::/s0 name::/s0/timer"
-VMMARGS="PC_PS2 kbmodifier:0x40000 name::/s0/log name::/s0/timer"
+SARGS="S0_DEFAULT hostvga:0x40000,0x40000 hostkeyb:0,0x60,1,12 tracebuffer:10240 vdisk:rom:///linux/grml.iso"
+SARGS_K="S0_DEFAULT hostvga:0x40000,0x40000 hostkeyb:0,0x60,1,12,1,1 tracebuffer:10240 vdisk:rom:///linux/grml.iso "
+
+BINARIES=" /nul/tutor_nul.gz --- /nul/cycleburner_nul.gz  --- /nul/rocknshine_nul.gz --- /nul/eurosys.slides  --- /nul/vancouver_nul.gz  --- /tools/munich --- /linux/bzImage.x --- /linux/bzImage --- /linux/initrd  --- /linux/grml.iso  --- /pistachio/i686_kernel  --- /pistachio/kickstart  --- /pistachio/sigma0  --- /pistachio/pingpong  --- /fiasco/fiasco  --- /fiasco/bootstrap  --- /fiasco/pingpong  --- /fiasco/roottask  --- /fiasco/sigma0  --- /tutor.nulconfig --- /linux.nulconfig  --- /grml.nulconfig  --- /pistachio/pistachio.nulconfig  --- /fiasco/fiasco.nulconfig  --- /cycleburner.nulconfig --- /rocknshine.nulconfig "
+
+BINARIES_NOVESA=" /nul/tutor_nul.gz --- /nul/cycleburner_nul.gz  --- /nul/rocknshine_nul.gz --- /nul/eurosys.slides  --- /nul/vancouver_nul.gz  --- /tools/munich --- /linux/bzImage.x --- /linux/bzImage --- /linux/initrd  --- /linux/grml.iso  --- /pistachio/i686_kernel  --- /pistachio/kickstart  --- /pistachio/sigma0  --- /pistachio/pingpong  --- /fiasco/fiasco  --- /fiasco/bootstrap  --- /fiasco/pingpong  --- /fiasco/roottask  --- /fiasco/sigma0  --- /tutor.nulconfig --- /linux_novesa.nulconfig  --- /grml.nulconfig  --- /pistachio/pistachio.nulconfig  --- /fiasco/fiasco.nulconfig  --- /cycleburner.nulconfig --- /rocknshine.nulconfig "
 
 cat > $ISO_DIR/isolinux/isolinux.cfg <<EOF
 
@@ -73,28 +67,64 @@ DEFAULT menu
 
 MENU HELPMSGROW 21
 MENU HELPMSGENDROW -2
-MENU TITLE NOVA snapshot 2010.12
+MENU TITLE NOVA Userland 2011.2
 
- LABEL novabasic
- MENU LABEL NOVA
+ LABEL nova1
+ MENU LABEL NOVA Userland Demo (VESA)
  KERNEL mboot.c32
- APPEND /tools/bender --- \
-        /tools/santamonica --- \
-        /nul/hypervisor nospinner dmar serial --- \
-        /nul/sigma0_nul.gz $SARGS --- \
-        /nul/vancouver_nul.gz $VMMARGS dpci:2,0,0 sigma0::dma sigma0::mem:512 --- \
-        /tools/munich sigma0::attach --- \
-        /demo/linux/bzImage root=/dev/ram0 clocksource=tsc sigma0::attach --- \
-        /demo/linux/initrd sigma0::attach 
+ APPEND /tools/unzip --- \
+        /tools/bender.gz --- \
+        /nul/hypervisor spinner iommu --- \
+        /nul/sigma0_nul.gz $SARGS hostvesa script_start:1 --- \
+        $BINARIES
 TEXT HELP
-Starts NOVA and one Linux VM that gets the first network card directly
-assigned. Use Win-[arrow] to navigate between hypervisor consoles.
+Needs VT-x/VT-d for full functionality.
 ENDTEXT
+
+ LABEL nova2
+ MENU LABEL NOVA Userland Demo (No VESA)
+ KERNEL mboot.c32
+ APPEND /tools/unzip --- \
+        /tools/bender.gz --- \
+        /nul/hypervisor spinner iommu --- \
+        /nul/sigma0_nul.gz $SARGS script_start:1 --- \
+        $BINARIES_NOVESA
+TEXT HELP
+Use this if graphics are garbled.
+ENDTEXT
+
+ LABEL nova3
+ MENU LABEL NOVA Userland Demo (VESA, broken keyboard)
+ KERNEL mboot.c32
+ APPEND /tools/unzip --- \
+        /tools/bender.gz --- \
+        /nul/hypervisor spinner iommu --- \
+        /nul/sigma0_nul.gz $SARGS_K hostvesa script_start:1 --- \
+        $BINARIES
+TEXT HELP
+Use this if your BIOS has weird USB legacy support.
+ENDTEXT
+
+
+ LABEL nova4
+ MENU LABEL NOVA Userland Demo (No VESA, broken keyboard)
+ KERNEL mboot.c32
+ APPEND /tools/unzip --- \
+        /tools/bender.gz --- \
+        /nul/hypervisor spinner iommu --- \
+        /nul/sigma0_nul.gz $SARGS_K script_start:1 --- \
+        $BINARIES_NOVESA
+TEXT HELP
+You poor soul. :-)
+ENDTEXT
+
 EOF
 
 mkisofs -o $TMPDIR/nul.iso -iso-level 2 \
         -b isolinux/isolinux.bin -c isolinux/boot.cat \
         -no-emul-boot -boot-load-size 4 -boot-info-table \
         $ISO_DIR || exit 4
+
+isohybrid $TMPDIR/nul.iso
 
 # EOF
