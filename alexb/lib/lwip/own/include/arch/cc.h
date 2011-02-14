aaca3300de9faac538a30ddf68375d4cfc712bb7
Julian Stecklina <js@alien8.de>
2011-02-14 11:46:16 +0100

    First round of code removal.

--- alexb/lib/lwip/own/include/arch/cc.h
+++
@@ -1,4 +0,0 @@
-#define BYTE_ORDER LITTLE_ENDIAN
-#define PACK_STRUCT_STRUCT __attribute__((packed))
-#define LWIP_COMPAT_MUTEX 1
-#define LWIP_PROVIDE_ERRNO
--- alexb/lib/lwip/own/include/arch/perf.h
+++
@@ -1,2 +0,0 @@
-#define PERF_START
-#define PERF_STOP(message) while (0) {}
