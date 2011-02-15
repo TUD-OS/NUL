4f3a7682f9587b8834f085ae4f12d83db1b1ea41
Julian Stecklina <js@alien8.de>
2011-02-15 16:05:54 +0100

    Revert "libruntime not needed in this release."
    This reverts commit e7f98da67b9ce06ec9cd788b9a0e908a978efcd4.
    
    cycleburner needs this with some gcc versions.

---
+++ vancouver/lib/runtime/cpp-support.cc
@@ -0,0 +1,22 @@
+/**
+ * C++ Runtime Stubs
+ *
+ * Copyright (C) 2010, Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of Vancouver.
+ *
+ * Vancouver is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * Vancouver is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
+extern "C" void __cxa_pure_virtual(void) __attribute__((noreturn));
+extern "C" void __cxa_pure_virtual(void) { __builtin_trap(); }
+
+// EOF
