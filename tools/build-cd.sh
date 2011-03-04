8fccff05a99108d095c83ae6f804f137ac7fe5db
Julian Stecklina <js@alien8.de>
2011-03-04 17:58:48 +0100

    Final CD building script

--- tools/build-cd.sh
+++ tools/build-cd.sh
@@ -39,27 +39,26 @@ mkdir -p $ISO_DIR/linux || exit 4
 cat $BINARIES/tutor.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' > $ISO_DIR/tutor.template || exit 4
 cat $BINARIES/rocknshine.nulconfig | sed 's,rom://nul/r,rom:///nul/r,' | sed 's,rom://nul/e,rom:///nul/e,' | sed 's/\.nul\.gz/_nul.gz/' > $ISO_DIR/rocknshine.template || exit 4
 cat $BINARIES/cycleburner.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' > $ISO_DIR/cycleburner.template || exit 4
-cat $BINARIES/grml.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' > $ISO_DIR/grml.template || exit 4
-cat $BINARIES/linux.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' | sed 's/initrd-udo/initrd/' > $ISO_DIR/linux.template || exit 4
-cat $ISO_DIR/linux.template | sed 's/bzImage\.x/bzImage/' > $ISO_DIR/linux_novesa.template
+#cat $BINARIES/grml.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' > $ISO_DIR/grml.template || exit 4
+cat $BINARIES/linux.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' | sed 's/initrd-udo/initrd/' > $ISO_DIR/linux.nulconfig || exit 4
 cat $BINARIES/pistachio/pistachio.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' | sed 's/i686-kernel/i686_kernel/' > $ISO_DIR/pistachio/pistachio.template
 cat $BINARIES/fiasco/fiasco.nulconfig | sed 's,rom://,rom:///,' | sed 's/\.nul\.gz/_nul.gz/' > $ISO_DIR/fiasco/fiasco.template
 echo > $ISO_DIR/empty.nulconfig
 
 
 #cp $BINARIES/linux/grml.iso $ISO_DIR/linux/ || exit 4
-dd if=/dev/zero bs=1M count=16 > $ISO_DIR/linux/grml.iso
+#dd if=/dev/zero bs=1M count=16 > $ISO_DIR/linux/grml.iso
 cp $BINARIES/nul/eurosys.slides $ISO_DIR/nul/ || exit 4
 cp $BINARIES/linux/bzImage.x $ISO_DIR/linux/bzImage.x || exit 4
 cp $BINARIES/linux/bzImage $ISO_DIR/linux/bzImage || exit 4
-cp $BINARIES/linux/initrd-udo $ISO_DIR/linux/initrd || exit 4
+cp $BINARIES/linux/initramfs $ISO_DIR/linux/initrd || exit 4
 
 # ISOLINUX config
 
 #SARGS="S0_DEFAULT hostvga:0x40000,0x40000 hostkeyb:0,0x60,1,12 tracebuffer:10240 vdisk:rom:///linux/grml.iso hostne2k "
-SARGS_K="S0_DEFAULT hostvga:0x40000,0x40000 hostkeyb:0,0x60,1,12,1,1 tracebuffer:10240 vdisk:rom:///linux/grml.iso nubus host82576:0 host82576vf:0,0 hostne2k service_config script_start:1"
+SARGS_K="S0_DEFAULT kiosk_mode hostvga:0x40000,0x40000 hostkeyb:0,0x60,1,12,1,1 tracebuffer:10240 vdisk:rom:///linux/grml.iso service_config hostserial script_start:1 script_wait:5000 script_start:2"
 
-BINARIES=" /nul/cycleburner_nul.gz  --- /nul/rocknshine_nul.gz --- /nul/eurosys.slides  --- /nul/vancouver_nul.gz --- /nul/remote_config_nul.gz --- /tools/munich --- /linux/bzImage --- /linux/bzImage --- /linux/initrd  --- /linux/grml.iso  --- /pistachio/i686_kernel  --- /pistachio/kickstart  --- /pistachio/sigma0  --- /pistachio/pingpong  --- /fiasco/fiasco  --- /fiasco/bootstrap  --- /fiasco/pingpong  --- /fiasco/roottask  --- /fiasco/sigma0 --- /linux.template  --- /grml.template  --- /pistachio/pistachio.template  --- /fiasco/fiasco.template  --- /cycleburner.template --- /rocknshine.template --- /empty.nulconfig sigma0::mem:12 name::/s0/timer name::/s0/config name::/s0/fs/rom || rom:///nul/remote_config_nul.gz linux:rom:///linux.template  grml:rom:///grml.template fiasco:rom:///fiasco/fiasco.template pistachio:rom:///pistachio/pistachio.template cycleburner:rom:///cycleburner.template"
+BINARIES=" /nul/cycleburner_nul.gz  --- /nul/rocknshine_nul.gz --- /nul/eurosys.slides  --- /nul/vancouver_nul.gz --- /nul/remote_config_nul.gz --- /tools/munich --- /linux/bzImage --- /linux/initrd  --- /pistachio/i686_kernel  --- /pistachio/kickstart  --- /pistachio/sigma0  --- /pistachio/pingpong  --- /fiasco/fiasco  --- /fiasco/bootstrap  --- /fiasco/pingpong  --- /fiasco/roottask  --- /fiasco/sigma0 --- /linux.nulconfig  --- /pistachio/pistachio.template  --- /fiasco/fiasco.template  --- /cycleburner.template --- /rocknshine.template --- /empty.nulconfig sigma0::mem:12 name::/s0/timer name::/s0/config name::/s0/fs/rom || rom:///nul/remote_config_nul.gz linux:rom:///linux.nulconfig fiasco:rom:///fiasco/fiasco.template pistachio:rom:///pistachio/pistachio.template cycleburner:rom:///cycleburner.template"
 
 cat > $ISO_DIR/isolinux/isolinux.cfg <<EOF
 
@@ -75,7 +74,7 @@ MENU TITLE NOVA Userland
  APPEND /tools/unzip --- \
         /tools/bender.gz --- \
         /nul/hypervisor spinner iommu --- \
-        /nul/sigma0_nul.gz $SARGS_K script_start:1 --- \
+        /nul/sigma0_nul.gz $SARGS_K --- \
         $BINARIES
 TEXT HELP
 Needs VT-x/VT-d for full functionality.
