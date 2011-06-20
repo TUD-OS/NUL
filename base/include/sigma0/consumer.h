a29573b7b83b315cb5c6d5bff6e31c28117cf2a3
Alexander Boettcher <boettcher@tudos.org>
2011-06-20 15:31:25 +0200

    make sure that everything is written before we signal a up

--- base/include/sigma0/consumer.h
+++ base/include/sigma0/consumer.h
@@ -72,6 +72,7 @@ public:
     _dropping = false;
     _consumer->_buffer[_consumer->_wpos] = value;
     _consumer->_wpos = (_consumer->_wpos + 1) % SIZE;
+    MEMORY_BARRIER;
     if (_sem.up(false)) Logging::printf("  : producer consumer issue - wake up failed\n");
     return true;
   }
@@ -164,6 +165,7 @@ public:
       Parent::_consumer->_wpos = 0;
     else
       Parent::_consumer->_wpos = ofs + needed;
+    MEMORY_BARRIER;
     if (Parent::_sem.up(false)) Logging::printf("  : producer consumer issue - wake up failed\n");
     return true;
   }
