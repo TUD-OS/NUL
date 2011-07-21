ec8dd0d3af1df6f1010a5cb4451f6902609ffa62
Michal Sojka <sojka@os.inf.tu-dresden.de>
2011-07-21 10:47:51 +0200

    Make Sigma0Base::requst_*_attach type safe and easier to understand

--- base/include/sigma0/sigma0.h
+++ base/include/sigma0/sigma0.h
@@ -24,76 +24,6 @@
 #include "nul/error.h"
 
 /**
- * This class defines the legacy call interface to sigma0 services.
- */
-class Sigma0Base : public BaseProgram
-{
- public:
-  enum {
-    REQUEST_STDIN_ATTACH = 0x1001,
-    REQUEST_DISKS_ATTACH,
-    REQUEST_NETWORK_ATTACH,
-    REQUEST_VNET_ATTACH,
-    REQUEST_DISK,
-    REQUEST_CONSOLE,
-    REQUEST_HOSTOP,
-    REQUEST_NETWORK,
-    REQUEST_PCICFG,
-    REQUEST_ACPI,
-    REQUEST_VNET,
-  };
- protected:
-
-  template <unsigned OP>
-    static unsigned request_attach(Utcb *utcb, void *buffer, unsigned sem_nq)
-    {
-      TemporarySave<Utcb::HEADER_SIZE + 5> save(utcb);
-
-      utcb->msg[0] = OP;
-      utcb->msg[1] = reinterpret_cast<unsigned long>(buffer);
-      utcb->set_header(2, 0);
-      /* Delegate sem to sigma0 with "up" permission */
-      add_mappings(utcb, sem_nq << Utcb::MINSHIFT, 1 << Utcb::MINSHIFT, MAP_MAP, 0x14 | DESC_TYPE_CAP);
-      check1(1, nova_call(14));
-      return utcb->msg[0];
-    }
-
-  template <class MESSAGE, unsigned OP>
-  static unsigned sigma0_message(MESSAGE &msg)
-  {
-    Utcb *utcb = myutcb();
-    assert(utcb);
-    const unsigned words = (sizeof(msg) +  sizeof(unsigned) - 1) / sizeof(unsigned);
-    TemporarySave<Utcb::HEADER_SIZE + 1 + words> save(utcb);
-    utcb->msg[0] = OP;
-    memcpy(utcb->msg + 1, &msg,  words*sizeof(unsigned));
-    utcb->set_header(1 + words, 0);
-    if (nova_call(14))
-      Logging::printf("sigma0 request failed %x\n", utcb->msg[0]);
-    memcpy(&msg,  utcb->msg + 1, words*sizeof(unsigned));
-    return utcb->msg[0];
-  }
- public:
-  struct PutsRequest
-  {
-    char *buffer;
-    PutsRequest(char *_buffer) : buffer(_buffer) {}
-  };
-  static unsigned  request_stdin         (Utcb *utcb, void *buffer, unsigned sem_nq) { return request_attach<REQUEST_STDIN_ATTACH>(utcb, buffer, sem_nq); }
-  static unsigned  request_disks_attach  (Utcb *utcb, void *buffer, unsigned sem_nq) { return request_attach<REQUEST_DISKS_ATTACH>(utcb, buffer, sem_nq); }
-  static unsigned  request_network_attach(Utcb *utcb, void *buffer, unsigned sem_nq) { return request_attach<REQUEST_NETWORK_ATTACH>(utcb, buffer, sem_nq); }
-  static unsigned  request_vnet_attach   (Utcb *utcb, unsigned sem_nq) { return request_attach<REQUEST_VNET_ATTACH>(utcb, NULL, sem_nq); }
-  static unsigned disk(MessageDisk &msg)     { return sigma0_message<MessageDisk,      REQUEST_DISK>(msg); }
-  static unsigned console(MessageConsole &msg)  { return sigma0_message<MessageConsole,   REQUEST_CONSOLE>(msg); }
-  static unsigned hostop (MessageHostOp &msg)   { return sigma0_message<MessageHostOp,    REQUEST_HOSTOP>(msg); }
-  static unsigned network(MessageNetwork &msg)  { return sigma0_message<MessageNetwork,   REQUEST_NETWORK>(msg); }
-  static unsigned pcicfg (MessagePciConfig &msg){ return sigma0_message<MessagePciConfig, REQUEST_PCICFG>(msg); }
-  static unsigned acpi   (MessageAcpi &msg)     { return sigma0_message<MessageAcpi,      REQUEST_ACPI>(msg); }
-  static unsigned vnetop (MessageVirtualNet &msg){ return sigma0_message<MessageVirtualNet,REQUEST_VNET>(msg); }
-};
-
-
-/**
  * Push interface sizes.
  */
 enum {
@@ -122,3 +52,75 @@ typedef Producer<MessageDiskCommit, DISKS_SIZE> DiskProducer;
  */
 typedef PacketConsumer<NETWORK_SIZE> NetworkConsumer;
 typedef PacketProducer<NETWORK_SIZE> NetworkProducer;
+
+/**
+ * This class defines the legacy call interface to sigma0 services.
+ */
+class Sigma0Base : public BaseProgram
+{
+ public:
+  enum {
+    REQUEST_STDIN_ATTACH = 0x1001,
+    REQUEST_DISKS_ATTACH,
+    REQUEST_NETWORK_ATTACH,
+    REQUEST_VNET_ATTACH,
+    REQUEST_DISK,
+    REQUEST_CONSOLE,
+    REQUEST_HOSTOP,
+    REQUEST_NETWORK,
+    REQUEST_PCICFG,
+    REQUEST_ACPI,
+    REQUEST_VNET,
+  };
+ protected:
+
+  /**
+   * Creates a producer in Sigma0 and attaches it to a consumer in the calling PD.
+   */
+  template <unsigned OP>
+    static unsigned request_attach(Utcb *utcb, void *consumer, unsigned sem_nq)
+    {
+      TemporarySave<Utcb::HEADER_SIZE + 5> save(utcb);
+
+      utcb->msg[0] = OP;
+      utcb->msg[1] = reinterpret_cast<unsigned long>(consumer);
+      utcb->set_header(2, 0);
+      /* Delegate sem to sigma0 with "up" permission */
+      add_mappings(utcb, sem_nq << Utcb::MINSHIFT, 1 << Utcb::MINSHIFT, MAP_MAP, 0x14 | DESC_TYPE_CAP);
+      check1(1, nova_call(14));
+      return utcb->msg[0];
+    }
+
+  template <class MESSAGE, unsigned OP>
+  static unsigned sigma0_message(MESSAGE &msg)
+  {
+    Utcb *utcb = myutcb();
+    assert(utcb);
+    const unsigned words = (sizeof(msg) +  sizeof(unsigned) - 1) / sizeof(unsigned);
+    TemporarySave<Utcb::HEADER_SIZE + 1 + words> save(utcb);
+    utcb->msg[0] = OP;
+    memcpy(utcb->msg + 1, &msg,  words*sizeof(unsigned));
+    utcb->set_header(1 + words, 0);
+    if (nova_call(14))
+      Logging::printf("sigma0 request failed %x\n", utcb->msg[0]);
+    memcpy(&msg,  utcb->msg + 1, words*sizeof(unsigned));
+    return utcb->msg[0];
+  }
+ public:
+  struct PutsRequest
+  {
+    char *buffer;
+    PutsRequest(char *_buffer) : buffer(_buffer) {}
+  };
+  static unsigned  request_stdin         (Utcb *utcb, StdinConsumer   *consumer, unsigned sem_nq) { return request_attach<REQUEST_STDIN_ATTACH>(utcb, consumer, sem_nq); }
+  static unsigned  request_disks_attach  (Utcb *utcb, DiskConsumer    *consumer, unsigned sem_nq) { return request_attach<REQUEST_DISKS_ATTACH>(utcb, consumer, sem_nq); }
+  static unsigned  request_network_attach(Utcb *utcb, NetworkConsumer *consumer, unsigned sem_nq) { return request_attach<REQUEST_NETWORK_ATTACH>(utcb, consumer, sem_nq); }
+  static unsigned  request_vnet_attach   (Utcb *utcb, unsigned sem_nq) { return request_attach<REQUEST_VNET_ATTACH>(utcb, NULL, sem_nq); }
+  static unsigned disk(MessageDisk &msg)     { return sigma0_message<MessageDisk,      REQUEST_DISK>(msg); }
+  static unsigned console(MessageConsole &msg)  { return sigma0_message<MessageConsole,   REQUEST_CONSOLE>(msg); }
+  static unsigned hostop (MessageHostOp &msg)   { return sigma0_message<MessageHostOp,    REQUEST_HOSTOP>(msg); }
+  static unsigned network(MessageNetwork &msg)  { return sigma0_message<MessageNetwork,   REQUEST_NETWORK>(msg); }
+  static unsigned pcicfg (MessagePciConfig &msg){ return sigma0_message<MessagePciConfig, REQUEST_PCICFG>(msg); }
+  static unsigned acpi   (MessageAcpi &msg)     { return sigma0_message<MessageAcpi,      REQUEST_ACPI>(msg); }
+  static unsigned vnetop (MessageVirtualNet &msg){ return sigma0_message<MessageVirtualNet,REQUEST_VNET>(msg); }
+};
