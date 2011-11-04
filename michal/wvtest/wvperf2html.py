7de9d0b40e9778ec9038bfe236f914cfccc8f8f6
Michal Sojka <sojka@os.inf.tu-dresden.de>
2011-11-04 17:07:22 +0100

    Add basicperf test

--- michal/wvtest/wvperf2html.py
+++ michal/wvtest/wvperf2html.py
@@ -106,7 +106,7 @@ graphs = Graphs()
 commits = {}
 
 re_date = re.compile('^Date: (.*)')
-re_testing = re.compile('^(\([0-9]+\) )?\s*Testing "(.*)" in (.*):\s*$')
+re_testing = re.compile('^(\([0-9]+\) (#   )?)?\s*Testing "(.*)" in (.*):\s*$')
 re_commit = re.compile('(\S+) (.*?), commit: (.*)')
 re_check = re.compile('^(\([0-9]+\) (#   )?)?!\s*(.*?)\s+(\S+)\s*$')
 re_perf =  re.compile('^(\([0-9]+\) (#   )?)?!\s*(.*?)\s+PERF:\s*(.*?)\s+(\S+)\s*$')
@@ -123,8 +123,8 @@ for line in sys.stdin.readlines():
     
     match = re_testing.match(line)
     if match:
-        what = match.group(2)
-        where = match.group(3)
+        what = match.group(3)
+        where = match.group(4)
 
         match = re_commit.match(what)
         if match:
