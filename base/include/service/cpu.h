e19c83233db191288cb4aa9076d9039eeaaa804d
Michal Sojka <sojka@os.inf.tu-dresden.de>
2011-11-15 15:52:01 +0100

    Document Cpu::minshift()

--- base/include/service/cpu.h
+++ base/include/service/cpu.h
@@ -74,6 +74,15 @@ class Cpu
   static  unsigned bsr(unsigned value) { return __builtin_clz(value) ^ 0x1F; }
   static  unsigned bsf(unsigned value) { return __builtin_ctz(value); }
 
+  /**
+   * Calculates the order (log2 of the size) of the largest naturally
+   * aligned block that starts at start and is no larger than size.
+   *
+   * @param minshift The largest value returned by this function.
+   *
+   * @return The calculated order or the minshift parameter is it is
+   * smaller then the order.
+   */
   static  unsigned minshift(unsigned long start, unsigned long size, unsigned minshift = 31) {
     unsigned shift = Cpu::bsf(start | (1ul << (8*sizeof(unsigned long)-1)));
     if (shift < minshift) minshift = shift;
