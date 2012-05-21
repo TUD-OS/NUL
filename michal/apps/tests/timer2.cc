22dd58a60109b8fb9f6742fbdf661ea155249acc
Alexander Boettcher <boettcher@tudos.org>
2012-05-21 09:27:45 +0200

    merge with timer.cc

--- michal/apps/tests/timer2.cc
+++ michal/apps/tests/timer2.cc
@@ -3,15 +3,17 @@
  * Test application for disk service.
  *
  * Copyright (C) 2011 Michal Sojka <sojka@os.inf.tu-dresden.de>
+ * Copyright (C) 2012 Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
+ * Copyright (C) 2012 Alexander Boettcher <boettcher@tudos.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
- * This file is part of Vancouver.nova.
+ * This file is part of NUL (NOVA user land).
  *
- * Vancouver.nova is free software: you can redistribute it and/or
+ * NUL is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
  * 2 as published by the Free Software Foundation.
  *
- * Vancouver.nova is distributed in the hope that it will be useful,
+ * NUL is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License version 2 for more details.
@@ -25,24 +27,29 @@ class TimerTest : public WvProgram
 public:
   void wvrun(Utcb *utcb, Hip *hip)
   {
-    Clock *clock = new Clock(hip->freq_tsc * 1000);
+    Clock *clock = new Clock((hip->freq_tsc + 1) * 1000); //freq_tsc is rounded down, add 1KHz to program it not before our intended point in time
     TimerProtocol *timer_service = new TimerProtocol(alloc_cap_region(TimerProtocol::CAP_SERVER_PT + hip->cpu_desc_count(), 0));
 
     WVPASS(clock);
     WVPASS(timer_service);
 
-    timevalue t1, t2;
+    timevalue t1, t2, t1_tsc, t2_tsc;
     timevalue ts;
 
     WVNUL(timer_service->time(*utcb, t1, ts));
-    WVNUL(timer_service->timer(*utcb, clock->abstime(100, 1000))); // 100 ms
+    t1_tsc = clock->time();
+    WVNUL(timer_service->timer(*utcb, clock->abstime(1, 1))); // 1s
     KernelSemaphore timersem = KernelSemaphore(timer_service->get_notify_sm());
 
     timersem.downmulti();
+    t2_tsc = clock->time();
     WVNUL(timer_service->time(*utcb, t2, ts));
-    timevalue sleep_time_ms = (t2 - t1 /* us */) / 1000;
+    timevalue sleep_time_us_service = (t2 - t1 /* us */);
+    timevalue sleep_time_us_tsc     = clock->clock(1000000, t2_tsc - t1_tsc);
 
-    WVPASSGE(static_cast<int>(sleep_time_ms), 100); // Broken in qemu
+    Logging::printf("Slept: service->time()=%lluus, tsc_time=%lluus \n", sleep_time_us_service, sleep_time_us_tsc);
+    WVPASSGE(static_cast<int>(sleep_time_us_service), 1000000); // Broken in qemu
+    //WVPASSGE(static_cast<int>(sleep_time_us_tsc), 1000000); // Broken in qemu //drift to large between service(HPET) and tsc
   }
 };
 
