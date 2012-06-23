4e55f042298b52bfafdfab8599fc32f5d6caaba4
Michal Sojka <sojka@os.inf.tu-dresden.de>
2012-06-23 16:05:38 +0200

    wvtest: Allow to limit the number of lines printed for failed tests
    And use this for nightly tests.

--- michal/wvtest/nul-nightly-cron.sh
+++ michal/wvtest/nul-nightly-cron.sh
@@ -33,7 +33,7 @@ Full log can be found at erwin:$PWD/$log
 
 EOF
 	export COLUMNS=100
-	cat $log | tr -d '\015' | wvformat | wvwrap #| iconv -f ASCII -t ASCII//IGNORE
+	cat $log | tr -d '\015' | wvformat --before-failure=10 | wvwrap #| iconv -f ASCII -t ASCII//IGNORE
     ) | /usr/sbin/sendmail -ti
 fi
 
